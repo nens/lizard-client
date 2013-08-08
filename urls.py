7753a9d6e23ee36f61225c011c46837e843bef45
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-08-08 13:42:15 +0200

    support the new client/server separation in all imports

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, patterns
 from django.conf.urls.defaults import include
 
 urlpatterns = patterns('',
-    url(r'^$', 'client.views.index', name='index'),
-    url(r'^search/', 'client.views.search', name='search'),
-    url(r'^jsonp/.*$', 'client.views.jsonp_view', name='jsonp'),
+    url(r'^$', 'lizard_nxt.client.views.index', name='index'),
+    url(r'^search/', 'lizard_nxt.client.views.search', name='search'),
+    url(r'^jsonp/.*$', 'lizard_nxt.client.views.jsonp_view', name='jsonp'),
 )
