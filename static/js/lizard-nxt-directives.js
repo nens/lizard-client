8cc53fb2aa138d34ade9c2f798ef85b6d40de157
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-09-02 16:25:17 +0200

    ...

--- static/js/lizard-nxt-directives.js
+++ static/js/lizard-nxt-directives.js
@@ -1,1 +1,30 @@
-/* Move directives here if they grow in number */
+app.directive('ngHistogram', function(){
+  // turns the <histogram/> element into an interactive crossfilter
+  // depends on crossfilter.js
+  return function($scope, element){
+    element[0].focus();
+  };
+});
+
+
+
+
+app.directive('ngFocus', function(){
+  // focus()es on the element you put this directive on
+  return function($scope, element){
+    element[0].focus();
+  };
+});
+
+app.directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if(event.which === 13) {
+                scope.$apply(function(){
+                    scope.$eval(attrs.onEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
+});
