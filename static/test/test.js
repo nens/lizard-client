46cc648cdda6a5e7020e1c7d93792c6939d8d09e
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2013-09-03 12:13:34 +0200

    testing and gruntfile almost ready for use, moved all the stuffs

--- static/test/test.js
+++ static/test/test.js
@@ -1,18 +1,8 @@
-var assert = require("assert");
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
   });
 });
 
-describe('Piet', function(){
-	describe("iets", function(){
-		it('shouldc bsldfl,', function(){
-			assert.equal(1, 1);
-		})
-	})
-})
