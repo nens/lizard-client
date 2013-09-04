1fc73672ea5f8c5bac757c45d4de60cf4a06c6a1
Ernst Kuilder <ernst.kuilder@nelen-schuurmans.nl>
2013-09-04 14:57:25 +0200

    merge conflicts and moved files

---
+++ static/source/app/lizard-nxt-services.js
@@ -0,0 +1,90 @@
+var services = angular.module("lizard-nxt.services", ['ngResource']);
+
+services.service("Cabinet", ["$resource", "$rootScope", 
+  function($resource, $rootScope) {
+
+  var layergroups = [];
+
+  var baselayers = [
+      {
+        active: true,
+        id: 1,
+        name: "Open Street Map",
+        url: "http://dev1.nxt.lizard.net:9000/osm_nens/{z}/{x}/{y}.png",
+        leafletLayer: L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
+      }];
+
+  apiLayerGroups = $resource('/api/v1/layergroups//:id/', 
+    {
+      id:'@id'
+    }, {
+      'query': {method: "GET", isArray:false}
+    });
+
+  apiLayerGroups.query(function(response) {
+    angular.copy(response.results, layergroups);
+     
+  // Default to inactive and ensure it has a leaflet layer
+    for (var i = 0; i < layergroups.length; i ++) {
+      var layergroup = layergroups[i];
+      for (var j = 0; j < layergroup.layers.length; j ++) {
+        var layer = layergroup.layers[j];
+        if (layer.active != true) {
+          layer.active = false;
+        }
+        if (layer.leafletLayer === undefined) {
+          layer.leafletLayer = L.tileLayer(layer.url);
+        }
+      };
+    };
+    $rootScope.$broadcast('LayersRetrieved');
+  });
+
+  return {
+    layergroups: layergroups,
+    baselayers: baselayers
+  }
+
+}]);
+
+
+services.service("leaflet", ["$rootScope", "Cabinet", function($rootScope, Cabinet) {
+  var map = L.map('map', {
+    center: new L.LatLng(52.0992287, 5.5698782),
+    zoomControl: false,
+    zoom: 8,
+  });
+
+  function addDefaultLayers(layers) {
+    for (var i = 0; i < layers.length; i ++) {
+    var layer = layers[i];
+    if (layer.active === true) {
+      map.addLayer(layer.leafletLayer);
+      }
+    }
+  };
+
+  addDefaultLayers(Cabinet.baselayers);
+
+  var scope = $rootScope.$new();
+  scope.$on('LayersRetrieved', function(event, layer) {
+    console.log("layers retrieved");
+    for (var i = 0; i < Cabinet.layergroups.length; i ++) {
+      addDefaultLayers(Cabinet.layergroups[i]);
+    }
+  });
+
+  scope.$on('LayerSwitched', function(event, layer) {
+    console.log("layer switched");
+    if (layer.active === true) {
+      map.addLayer(layer.leafletLayer);
+    }
+    if (layer.active === false) {
+      map.removeLayer(layer.leafletLayer);
+    }
+  });
+
+  return{
+    map: map
+  }
+}]);
--- static/source/app/lizard-nxt.js
+++ static/source/app/lizard-nxt.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD:lizard_nxt/client/static/src/app/lizard-nxt.js
-var app = angular.module('lizard-nxt', ['lizard-nxt.services']);
-=======
 var app = angular.module("lizard-nxt", [
   'ngResource', 
   'ui.event', 
   'ui.highlight', 
   'ui.keypress',
-  'omnibox']);
->>>>>>> 5f552351eec475efdcdafa59eb79a7213f6c2dd9:lizard_nxt/client/static/source/app/lizard-nxt.js
+  'omnibox',
+  'lizard-nxt.services']);
 
 app.config(function($interpolateProvider) {
   //To prevent Django and Angular Template hell
@@ -16,33 +13,9 @@ app.config(function($interpolateProvider) {
  });
 
 
-<<<<<<< HEAD:lizard_nxt/client/static/src/app/lizard-nxt.js
 app.controller("MapLayerCtrl", ["$rootScope", "$scope", "Cabinet", function($rootScope, $scope, Cabinet) {
   $scope.layergroups = Cabinet.layergroups;
   $scope.baselayers = Cabinet.baselayers;
-=======
-app.service("Cabinet", ["$resource", "$rootScope",
-  function($resource, $rootScope) {
-
-  var layergroups,
-      apiLayerGroups;
-
-  apiLayerGroups = $resource('/api/v1/layergroups//:id/',
-    {
-      id:'@id'
-    }, {
-      'query': {method: "GET", isArray:false}
-    });
-
-  apiLayerGroups.query(function(response) {
-     layergroups = response.results;
-     $rootScope.$broadcast('layergroups', layergroups);
-  });
-  return {
-    layergroups: apiLayerGroups
-  };
-}]);
->>>>>>> 5f552351eec475efdcdafa59eb79a7213f6c2dd9:lizard_nxt/client/static/source/app/lizard-nxt.js
 
   $scope.switch = function(layer) {
     $rootScope.$broadcast('LayerSwitched', layer);
@@ -52,14 +25,7 @@ app.service("Cabinet", ["$resource", "$rootScope",
 app.controller("MapCtrl",
   ["$scope", "$rootScope", "leaflet", function($scope, $rootScope, leaflet) {
 
-<<<<<<< HEAD:lizard_nxt/client/static/src/app/lizard-nxt.js
     leaflet.map.on('click', function(e) {
         $rootScope.$broadcast('mapclick', e.latlng);
     });
-=======
-  $scope.$on('layergroups', function(message, content) {
-    $scope.layergroups = content;
-  });
->>>>>>> 5f552351eec475efdcdafa59eb79a7213f6c2dd9:lizard_nxt/client/static/source/app/lizard-nxt.js
-
 }]);
