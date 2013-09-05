f7c3916c8c243152c49874efa1735b82f354be00
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-09-05 12:27:49 +0200

    Forgot to add some more

---
+++ static/source/app/controllers/omnibox-controllers.js
@@ -0,0 +1,52 @@
+app.controller("SearchCtrl",
+    ["$scope", "$resource", "$rootScope", "Cabinet", "Omnibox",
+        function($scope, $resource, $rootScope, Cabinet, Omnibox) {
+
+    $scope.box = Omnibox;
+
+
+    $scope.filter = function ($event) {
+        if ($scope.box.query.length > 1) {
+
+            var search = Cabinet.search.get({q:$scope.box.query}, function(data) {
+                console.log(data.hits.hits);
+                var sources = [];
+                for(var i in data.hits.hits) {
+                    sources.push(data.hits.hits[i]._source);
+                }
+                $scope.searchData = sources;
+            });
+
+            var geocode = Cabinet.geocode.query({q:$scope.box.query}, function(data) {
+                console.log(data);
+                $scope.geocodeData = data;
+            });
+            $scope.box.open("location");
+            
+        }
+    };
+
+    $scope.reset_query = function() {
+        $scope.box.query = null;
+        $scope.box.close();
+    };
+
+
+
+}]);
+
+
+
+
+app.controller("CardsCtrl",
+    ["$scope", "$resource", "$rootScope", "Cabinet", "Omnibox",
+        function($scope, $resource, $rootScope, Cabinet, Omnibox) {
+
+
+    $scope.box = Omnibox;
+
+
+
+
+}]);
+
