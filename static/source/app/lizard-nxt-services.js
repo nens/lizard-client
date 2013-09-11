e09568384755c22cc9251bd1382473afd5df370c
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-09-11 12:52:57 +0200

    js hint

--- static/source/app/lizard-nxt-services.js
+++ static/source/app/lizard-nxt-services.js
@@ -29,13 +29,13 @@ services.service("Cabinet", ["$resource", "$rootScope",
     for (var j in layers){
       layerGroups[i].layers.map(function(id){
         if (id === layers[j].id){
-          grouplayers.push(layers[j]);      
+          grouplayers.push(layers[j]);
         }
-      })
+      });
     }
     new_layerGroup.layers = grouplayers;
     layergroups.push(new_layerGroup);
-  };
+  }
 
   $rootScope.$broadcast('LayersRetrieved');
 
@@ -44,79 +44,6 @@ services.service("Cabinet", ["$resource", "$rootScope",
     search: searchResource,
     geocode: geocodeResource,
     reverseGeocode: reverseGeocodeResource
-  };
-}]);
-
-
-services.service("leaflet", ["$rootScope", "Cabinet", function($rootScope, Cabinet) {
-  var map = L.map('map', {
-    center: new L.LatLng(52.0992287, 5.5698782),
-    zoomControl: false,
-    zoom: 8
-  });
-
-  function addDefaultLayers(layergroups) {
-    for (var i = 0; i < layergroups.length; i ++) {
-      var layergroup = layergroups[i];
-      for (var j = 0; j < layergroup.layers.length; j ++) {
-        var layer = layergroup.layers[j];
-        if (layer.leafletLayer === undefined) {
-          if (layer.type == "WMS") {
-            // TODO: fix something more robust for WMS layers.
-            // It works when the layer.url defines the layer name
-            // and the wms server is hardcoded
-            console.log(layer.type, layer);
-            wms = 'http://geoserver1-3di.lizard.net/geoserver/gwc/service/wms';
-            layer.leafletLayer = L.tileLayer.wms(wms, {
-              layers: layer.url,
-              format: 'image/png',
-              version: '1.1.1' });
-          }
-          else {
-            layer.leafletLayer = L.tileLayer(layer.url);
-          }
-        }
-        if (layer.active) {
-          map.addLayer(layer.leafletLayer);
-          if (layer.baselayer) {
-            layer.leafletLayer.bringToBack();
-            $rootScope.$broadcast('baselayerActive', layer.id);
-          }
-        }
-      }
-    }
-  }
-
-  var scope = $rootScope.$new();
-
-  scope.$on('LayersRetrieved', function(event, layer) {
-    addDefaultLayers(Cabinet.layergroups);
-  });
-
-  scope.$on('LayerSwitched', function(event, layer) {
-    console.log("layer switched");
-    if (layer.active === true) {
-      map.addLayer(layer.leafletLayer);
-    }
-    if (layer.active === false) {
-      map.removeLayer(layer.leafletLayer);
-    }
-  });
-
-  scope.$on('LayerOn', function(event, layer) {
-    map.addLayer(layer.leafletLayer);
-    if (layer.baselayer) {
-      layer.leafletLayer.bringToBack();
-    }
-  });
-
-  scope.$on('LayerOff', function(event, layer) {
-    map.removeLayer(layer.leafletLayer);
-  });
-
-
-  return{
-    map: map
   };
 }]);
 
@@ -133,7 +60,7 @@ services.service("Omnibox", [function() {
   box.open = function(type){
     box.type = type;
     box.showCards = true;
-    $rootScope.$broadcast('Omnibox_change')
+    $rootScope.$broadcast('Omnibox_change');
   };
 
   box.close = function(){
