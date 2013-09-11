19dda64f359caa042cf2d0d102fdde0fc1efe337
Gijs Nijholt <gijs.nijholt@gmail.com>
2013-09-11 10:57:43 +0200

    Something went wrong merging.. fixed

--- static/source/common/nxt_graph/nxt-graph.js
+++ static/source/common/nxt_graph/nxt-graph.js
@@ -127,126 +127,126 @@ app
           left: 45
       }, 
       width = 400 - margin.left - margin.right, 
-      height = 200 - margin.top - margin.bottom,
-      chart = function(){
-            var x = d3.time.scale()
-                .domain(d3.extent(data, function (d) {
-                return d.date;
-            }))
-                .range([0, width]);
+      height = 200 - margin.top - margin.bottom
+      
+      var x = d3.time.scale()
+          .domain(d3.extent(data, function (d) {
+          return d.date;
+      }))
+          .range([0, width]);
 
-            var y = d3.scale.linear()
-                .domain(d3.extent(data, function (d) {
-                return d.value;
-            }))
-            .range([height, 0]);
+      var y = d3.scale.linear()
+          .domain(d3.extent(data, function (d) {
+          return d.value;
+      }))
+      .range([height, 0]);
 
 
-        var line = d3.svg.line()
-            .x(function (d) {
-            return x(d.date);
-        })
-            .y(function (d) {
-            return y(d.value);
-        });
+      var line = d3.svg.line()
+          .x(function (d) {
+          return x(d.date);
+      })
+          .y(function (d) {
+          return y(d.value);
+      });
 
-        var zoom = d3.behavior.zoom()
-          .x(x)
-          .y(y)
-          .on("zoom", zoomed);
-        
-          // Make sure your context as an id or so...
-          console.log(element.context)
-          var svg = d3.select(element.context)
-            .append("svg:svg")
-            .attr('width', 400)
-            .attr('height', 230)
-            .append("svg:g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-            .call(zoom);
+      var zoom = d3.behavior.zoom()
+        .x(x)
+        .y(y)
+        .on("zoom", zoomed);
+      
+        // Make sure your context as an id or so...
+        var svg = d3.select(element.context)
+          .html("")
+          .append("svg:svg")
+          .attr('width', 500)
+          .attr('height', 300)
+          .append("svg:g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+          .call(zoom);
 
-            svg.append("svg:rect")
-              .attr("width", width)
-              .attr("height", height)
-              .attr("class", "plot");
+          svg.append("svg:rect")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("class", "plot");
 
-          var make_x_axis = function () {
-              return d3.svg.axis()
-                  .scale(x)
-                  .orient("bottom")
-                  .ticks(5);
-          };
+        var make_x_axis = function () {
+            return d3.svg.axis()
+                .scale(x)
+                .orient("bottom")
+                .ticks(5);
+        };
 
-          var make_y_axis = function () {
-              return d3.svg.axis()
-                  .scale(y)
-                  .orient("left")
-                  .ticks(5);
-          };
+        var make_y_axis = function () {
+            return d3.svg.axis()
+                .scale(y)
+                .orient("left")
+                .ticks(5);
+        };
 
-          var xAxis = d3.svg.axis()
-              .scale(x)
-              .orient("bottom")
-              .ticks(5);
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .ticks(5);
 
-          svg.append("svg:g")
-              .attr("class", "x axis")
-              .attr("transform", "translate(0, " + height + ")")
-              .call(xAxis);
+        svg.append("svg:g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0, " + height + ")")
+            .call(xAxis);
 
-          var yAxis = d3.svg.axis()
-              .scale(y)
-              .orient("left")
-              .ticks(5);
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(5);
 
-          svg.append("g")
-              .attr("class", "y axis")
-              .call(yAxis);
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
 
-          svg.append("g")
-              .attr("class", "x grid")
-              .attr("transform", "translate(0," + height + ")")
-              .call(make_x_axis()
-              .tickSize(-height, 0, 0)
-              .tickFormat(""));
+        svg.append("g")
+            .attr("class", "x grid")
+            .attr("transform", "translate(0," + height + ")")
+            .call(make_x_axis()
+            .tickSize(-height, 0, 0)
+            .tickFormat(""));
 
-          svg.append("g")
-              .attr("class", "y grid")
-              .call(make_y_axis()
-              .tickSize(-width, 0, 0)
-              .tickFormat(""));
+        svg.append("g")
+            .attr("class", "y grid")
+            .call(make_y_axis()
+            .tickSize(-width, 0, 0)
+            .tickFormat(""));
 
-          var clip = svg.append("svg:clipPath")
-              .attr("id", "clip")
-              .append("svg:rect")
-              .attr("x", 0)
-              .attr("y", 0)
-              .attr("width", width)
-              .attr("height", height);
+        var clip = svg.append("svg:clipPath")
+            .attr("id", "clip")
+            .append("svg:rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", width)
+            .attr("height", height);
 
-          var chartBody = svg.append("g")
-              .attr("clip-path", "url(#clip)");
+        var chartBody = svg.append("g")
+            .attr("clip-path", "url(#clip)");
 
-          chartBody.append("svg:path")
-              .datum(data)
-              .attr("class", "line")
-              .attr("d", line);
+        chartBody.append("svg:path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);
 
-          function zoomed() {
-              svg.select(".x.axis").call(xAxis);
-              svg.select(".y.axis").call(yAxis);
-              svg.select(".x.grid")
-                  .call(make_x_axis()
-                  .tickSize(-height, 0, 0)
-                  .tickFormat(""));
-              svg.select(".y.grid")
-                  .call(make_y_axis()
-                  .tickSize(-width, 0, 0)
-                  .tickFormat(""));
-              svg.select(".line")
-                  .attr("class", "line")
-                  .attr("d", line);
-          }
+        function zoomed() {
+            svg.select(".x.axis").call(xAxis);
+            svg.select(".y.axis").call(yAxis);
+            svg.select(".x.grid")
+                .call(make_x_axis()
+                .tickSize(-height, 0, 0)
+                .tickFormat(""));
+            svg.select(".y.grid")
+                .call(make_y_axis()
+                .tickSize(-width, 0, 0)
+                .tickFormat(""));
+            svg.select(".line")
+                .attr("class", "line")
+                .attr("d", line);
+        }
       };
   return {
     restrict: 'E',
