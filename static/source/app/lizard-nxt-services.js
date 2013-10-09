f5fce79f4b82a204ec5b2e3e038e1455fbe6fd88
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2013-10-09 11:42:55 +0200

    .. idiot

--- static/source/app/lizard-nxt-services.js
+++ static/source/app/lizard-nxt-services.js
@@ -21,8 +21,7 @@ services.service("CabinetService", ["$resource",
   reverseGeocodeResource = $resource('/api/v1/reversegeocode/');
   timeseriesLocationObjectResource = $resource('/api/v1/timeseries/?object=:object_type$:id&page_size=0', {
     object_type: '@object_type',
-    id: '@id'
-  }, {
+    id: '@id',
     isArray: true
   });
   // timeseriesResource = $resource('/api/v1/timeseries/:id/', {
