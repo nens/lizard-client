9d942f103998fad008aae0efb3f30d9089f9e220
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2013-11-19 12:10:15 +0100

    start with tempaltes etc

--- static/source/app/directives/raster-aggregate-directives.js
+++ static/source/app/directives/raster-aggregate-directives.js
@@ -1,9 +1,26 @@
 // raster-aggregate-directives.js
 
 app.directive('rasterAggregate', function () {
-    return {
-        restrict: 'E',
-        replace: true
-        templateUrl: 'templates/raster-aggregate.html'
-    }
+  var link = function (scope, element, attrs) {
+
+    var mapBounds = scope.map.getBounds();
+    var srs = "EPGS:4326";
+    var geom_wkt = "POLYGON(("
+            + mapBounds.getWest() + " " + mapBounds.getSouth() + ", "
+            + mapBounds.getEast() + " " + mapBounds.getSouth() + ", "
+            + mapBounds.getEast() + " " + mapBounds.getNorth() + ", "
+            + mapBounds.getWest() + " " + mapBounds.getNorth() + ", "
+            + mapBounds.getWest() + " " + mapBounds.getSouth()
+            + "))";
+
+    scope.getRasterData('landuse', geom_wkt, srs);
+    console.log('haha', scope.box.content);
+  };
+
+  return {
+    restrict: 'E',
+    replace: true,
+    link: link,
+    templateUrl: 'templates/raster-aggregate.html'
+  }
 });
