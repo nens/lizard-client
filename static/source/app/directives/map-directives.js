72675757a1f4ad010f8ad2085fadc39b4d347a1f
Gijs Nijholt <gijs.nijholt@gmail.com>
2014-01-28 13:44:47 +0100

    ...

--- static/source/app/directives/map-directives.js
+++ static/source/app/directives/map-directives.js
@@ -40,7 +40,6 @@ app
           }
           layer.leafletLayer = L.tileLayer.wms(layer.url, options);
         } else if (layer.type === "ASSET") {
-          console.log('layer:', layer);
           var url = '/api/v1/tiles/{slug}/{z}/{x}/{y}.{ext}';
           layer.grid_layers = [];
           for (var i in layer.sublayers) {
