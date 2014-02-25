b79ea884862ca23e086d9d95d965013b9f1cce90
Ernst Kuilder <ernst.kuilder@nelen-schuurmans.nl>
2014-02-25 14:32:02 +0100

    custom filter for cards

--- static/source/app/lizard-nxt-filters.js
+++ static/source/app/lizard-nxt-filters.js
@@ -10,6 +10,15 @@
  * 
  */
 
+
+/**
+ * Nice number or dash returns a rounded number or a '-'
+ *
+ * @param {string or number} input to round or convert to dash
+ * @param {number} optional decimals to round the number to
+ * @return {string or number} when input is a number: returns a number
+ * rounded to specified decimals else returns '-'
+ */
 app.filter('niceNumberOrDash', function () {
     return function (input, decimals) {
       var out;
