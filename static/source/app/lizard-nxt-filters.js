ff11b776fec022db4c8e16aa88a1b0fe4a2c316c
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2014-02-28 09:39:19 +0100

    Renamed nice number or dash to ellipsis

--- static/source/app/lizard-nxt-filters.js
+++ static/source/app/lizard-nxt-filters.js
@@ -12,14 +12,14 @@
 
 
 /**
- * Nice number or dash returns a rounded number or a '...'.
+ * Nice number or ellipsis returns a rounded number or a '...'.
  *
  * @param {string or number} input to round or convert to dash
  * @param {number} optional decimals to round the number to
  * @return {string or number} when input is a number: returns a number
  * rounded to specified decimals else returns '-'
  */
-app.filter('niceNumberOrDash', function () {
+app.filter('niceNumberOrEllipsis', function () {
     return function (input, decimals) {
       var out;
       if (typeof(input) === 'number') {
