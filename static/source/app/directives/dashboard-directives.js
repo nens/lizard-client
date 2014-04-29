29d9c8a41406448795cd688e36ba443d7e99f905
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2014-04-29 10:08:28 +0200

    isolate scope, remove alertme and tab to spaces

--- static/source/app/directives/dashboard-directives.js
+++ static/source/app/directives/dashboard-directives.js
@@ -2,27 +2,24 @@
 
 app.controller('DashboardDirCtrl', ['$scope', '$timeout', '$http', '$sce', function ($scope, $timeout, $http, $sce) {
 
-	/*
-	 * TODO: This should build the dashboard according to a JSON object
-	 * which could come from a DashboardService or something, which in turn
-	 * persists/keeps its configuration for this user/organisation using the API
-	 * 
-	 */
+  /*
+   * TODO: This should build the dashboard according to a JSON object
+   * which could come from a DashboardService or something, which in turn
+   * persists/keeps its configuration for this user/organisation using the API
+   *
+   * TODO: good candidate for ui-router.
+   * 
+   */
 
-	$scope.tabs = [{
-			title: "Streetview", 
-			content: $sce.trustAsHtml("<h4>Streetview</h4>")
-		},
-		{
-			title:"Extra",
-			content: $sce.trustAsHtml("<h4>Extra</h4><p>Dynamic content 2</p>"), 
-			disabled: true
-		}];
-
-	$scope.alertMe = function() {
-		// NOTE: Example of how a controller function can be called from the directive
-		return console.log('alertMe()');
-	};
+  $scope.tabs = [{
+      title: "Streetview", 
+      content: $sce.trustAsHtml("<h4>Streetview</h4>")
+    },
+    {
+      title:"Extra",
+      content: $sce.trustAsHtml("<h4>Extra</h4><p>Dynamic content 2</p>"), 
+      disabled: true
+    }];
 }]);
 
 app.directive('dashboard', ['$location', '$timeout', '$compile', function ($location, $timeout, $compile) {
@@ -32,6 +29,7 @@ app.directive('dashboard', ['$location', '$timeout', '$compile', function ($loca
 
   return {
       restrict: 'E',
+      scope: true,
       replace: true,
       controller: 'DashboardDirCtrl',
       templateUrl: 'templates/fullscreen.html',
