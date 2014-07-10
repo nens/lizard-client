f7be6326cc6599bb58ed7135e6fee4ca9ef33a59
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2014-07-10 16:16:20 +0200

    update hash when start or end changes

--- source/app/controllers/hash-controller.js
+++ source/app/controllers/hash-controller.js
@@ -3,22 +3,56 @@
 app.controller('hashGetterSetter', ["$scope", "hashSyncHelper",
   function ($scope, hashSyncHelper) {
 
+    // Only set url when user changed it or on pageload
     var updateUrl = true;
 
+    /**
+     * Set baselayer hash when activeBaselayer changed.
+     */
     $scope.$watch('mapState.activeBaselayer', function (n, o) {
       if (n === o) { return true; }
-      setBaselayerUrl();
+      setBaselayerUrl($scope.mapState.activeBaselayer);
     });
 
+    /**
+     * Set location hash when map moved.
+     */
     $scope.$watch('mapState.moved', function (n, o) {
       if (n === o) { return true; }
-      console.log('moved');
       updateUrl = false;
       setCoordinatesUrl();
     });
 
+    /**
+     * Set start hash when timeState.start changed.
+     */
+    $scope.$watch('timeState.start', function (n, o) {
+      if (n === o) { return true; }
+      updateUrl = false;
+      setTimeStateUrl($scope.timeState.start, true);
+    });
+
+    /**
+     * Set end hash when timeState.end changed.
+     */
+    $scope.$watch('timeState.end', function (n, o) {
+      if (n === o) { return true; }
+      updateUrl = false;
+      setTimeStateUrl($scope.timeState.end, false);
+    });
+
+    var setTimeStateUrl = function (time, start) {
+      var date = new Date(time);
+      var dateString = date.toDateString().split(' ').join('-');
+      if (start) {
+        hashSyncHelper.setHash({'start': dateString});
+      } else {
+        hashSyncHelper.setHash({'end': dateString});
+      }
+    };
+
     var setCoordinatesUrl = function () {
-      var COORD_PRECISION = 5;
+      var COORD_PRECISION = 4;
       var newHash = [
         $scope.map.getCenter().lat.toFixed(COORD_PRECISION),
         $scope.map.getCenter().lng.toFixed(COORD_PRECISION),
@@ -33,9 +67,23 @@ app.controller('hashGetterSetter', ["$scope", "hashSyncHelper",
       }
     };
 
-    var setBaselayerUrl = function () {
-      hashSyncHelper.setHash({'bl': $scope.mapState.activeBaselayer}); // set baselayer in url by id
+    var setBaselayerUrl = function (baselayerId) {
+      hashSyncHelper.setHash({'baselayer': baselayerId}); // set baselayer in url by id
     };
+
+    (function setUrlHashWhenEmpty() {
+      var hash = hashSyncHelper.getHash();
+
+      var baselayerHash = hash.baselayer;
+      var locationHash = hash.location;
+
+      if (!locationHash) {
+        setCoordinatesUrl();
+      }
+      if (!baselayerHash) {
+        setBaselayerUrl($scope.mapState.activeBaselayer);
+      }
+    })();
 
     /**
      * Listener to update map view when user changes url
@@ -45,12 +93,13 @@ app.controller('hashGetterSetter', ["$scope", "hashSyncHelper",
      * resetting the updateUrl back to true
      */
     $scope.$on('$locationChangeSuccess', function (e, oldurl, newurl) {
-      console.log(updateUrl);
       if (updateUrl) {
         var hash = hashSyncHelper.getHash();
 
-        var baselayerHash = hash.bl;
+        var baselayerHash = hash.baselayer;
         var locationHash = hash.location;
+        var startHash = hash.start;
+        var endHash = hash.end;
 
         if (baselayerHash !== undefined) {
           $scope.mapState.activeBaselayer = parseInt(baselayerHash, 10);
@@ -65,20 +114,17 @@ app.controller('hashGetterSetter', ["$scope", "hashSyncHelper",
             }
           }
         }
+
+        if (startHash !== undefined) {
+          $scope.timeState.start = Date.parse(startHash);
+        }
+
+        if (endHash !== undefined) {
+          $scope.timeState.end = Date.parse(endHash);
+        }
       }
       updateUrl = true;
     });
 
-    var hash = hashSyncHelper.getHash();
-
-    var baselayerHash = hash.bl;
-    var locationHash = hash.location;
-
-    if (!locationHash) {
-      setCoordinatesUrl();
-    }
-    if (!baselayerHash) {
-      setBaselayerUrl();
-    }
   }
 ]);
