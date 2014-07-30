6ba768a8cfed04fdcd053fce69f19a13e07a38f0
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2014-07-30 16:14:57 +0200

    refactor extentctrl and create and rm isolated scope for omnibox

--- source/app/controllers/raster-aggregate-controller.js
+++ source/app/controllers/raster-aggregate-controller.js
@@ -1,88 +1,95 @@
-app.controller("RasterAggregateCtrl", ["$scope", "$q", "RasterService",
-  function ($scope, $q, RasterService) {
-  /**
-   * ExtentAggregate is the object which collects different
-   * sets of aggregation data. If there is no activeObject,
-   * this is the default collection of data to be shown in the
-   * client.
-   *
-   * Contains: landuse, soil and elevation data.
-   *
-   */
+app.controller("RasterAggregateCtrl", [
+  "$scope",
+  "$q",
+  "RasterService",
+  "CardService",
+  function ($scope, $q, RasterService, CardService) {
+    /**
+     * ExtentAggregate is the object which collects different
+     * sets of aggregation data. If there is no activeObject,
+     * this is the default collection of data to be shown in the
+     * client.
+     *
+     * Contains: landuse, soil and elevation data.
+     *
+     */
 
-  $scope.extentAggregate = {
-    changed: true,
-    landuse: {
-      active: false,
-      data: [],
-      q: $q.defer()
-    },
-    soil: {
-      active: false,
-      data: [],
-      types: [],
-      q: $q.defer()
-    },
-    elevation: {
-      active: false,
-      data: [],
-      q: $q.defer()
-    }
-  };
+    $scope.extentAggregate = {
+      changed: true,
+      landuse: {
+        active: CardService.getPriority('landuse'),
+        data: [],
+        q: $q.defer()
+      },
+      soil: {
+        active: CardService.getPriority('soil'),
+        data: [],
+        types: [],
+        q: $q.defer()
+      },
+      elevation: {
+        active: CardService.getPriority('elevation'),
+        data: [],
+        q: $q.defer()
+      }
+    };
 
+    /**
+     * Cancels pending requests and refreshes $q type promises
+     */
+    var extAggPromiseRefresh = function () {
+      $scope.extentAggregate.landuse.q.resolve();
+      $scope.extentAggregate.elevation.q.resolve();
+      // $scope.extentAggregate.soil.q.resolve();
+      $scope.extentAggregate.landuse.q = $q.defer();
+      $scope.extentAggregate.elevation.q = $q.defer();
+      // $scope.extentAggregate.soil.q = $q.defer();
+    };
 
-  /**
-   * Cancels pending requests and refreshes $q type promises
-   */
-  $scope.extAggPromiseRefresh = function () {
-    $scope.extentAggregate.landuse.q.resolve();
-    $scope.extentAggregate.elevation.q.resolve();
-    // $scope.extentAggregate.soil.q.resolve();
-    $scope.extentAggregate.landuse.q = $q.defer();
-    $scope.extentAggregate.elevation.q = $q.defer();
-    // $scope.extentAggregate.soil.q = $q.defer();
-  };
+    var handleLanduseCount = function (data) {
+      $scope.extentAggregate.landuse.data = data;
+      $scope.extentAggregate.landuse.active = true;
+    };
 
-  $scope.handleLanduseCount = function (data) {
-    $scope.extentAggregate.landuse.data = data;
-    $scope.extentAggregate.landuse.active = true;
-  };
+    var getLanduseCount = function (geom) {
+      RasterService.getRasterData('landuse', geom, {
+        agg: 'counts',
+        q: $scope.extentAggregate.landuse.q
+      }).then(handleLanduseCount);
+    };
 
-  $scope.getLanduseCount = function (geom) {
-    RasterService.getRasterData('landuse', geom, {
-      agg: 'counts',
-      q: $scope.extentAggregate.landuse.q
-    }).then($scope.handleLanduseCount);
-  };
+    var handleElevationCurve = function (data) {
+      var datarow,
+          i,
+          formatted = [];
 
-  $scope.handleElevationCurve = function (data) {
-    var datarow,
-        i,
-        formatted = [];
-        
-    for (i in data[0]) {
-      datarow = [data[0][i], data[1][i]];
-      formatted.push(datarow);
-    }
-    $scope.extentAggregate.elevation.data = formatted;
-  };
+      for (i in data[0]) {
+        datarow = [data[0][i], data[1][i]];
+        formatted.push(datarow);
+      }
+      $scope.extentAggregate.elevation.data = formatted;
+    };
 
-  $scope.getElevationCurve = function (geom) {
-    RasterService.getRasterData('elevation', geom, {
-      agg: 'curve',
-      q: $scope.extentAggregate.elevation.q
-    }).then($scope.handleElevationCurve);
+    var getElevationCurve = function (geom) {
+      RasterService.getRasterData('elevation', geom, {
+        agg: 'curve',
+        q: $scope.extentAggregate.elevation.q
+      }).then(handleElevationCurve);
 
-  };
+    };
 
-  var mapWatch = $scope.$watch('mapState.bounds', function (newVal, oldVal) {
+    var mapWatch = $scope.$watch('mapState.bounds', function (newVal, oldVal) {
 
-    $scope.extAggPromiseRefresh();
+      extAggPromiseRefresh();
 
-    var geom = $scope.mapState.bounds;
+      var geom = $scope.mapState.bounds;
 
-    $scope.getLanduseCount(geom);
-    $scope.getElevationCurve(geom);
-  });
- 
-}]);
+      if ($scope.extentAggregate.landuse.active) {
+        getLanduseCount(geom);
+      }
+      if ($scope.extentAggregate.soil.active) {
+        getElevationCurve(geom);
+      }
+    });
+  }
+]);
