3fb7c2136c949658a2f9e4012c03a439e76fe306
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2014-08-26 17:19:05 +0200

    Create new graph directive

--- source/app/services/graph-service.js
+++ source/app/services/graph-service.js
@@ -11,30 +11,23 @@
  *
  * Everything in the graphs is animated for 500 milliseconds.
  */
-app.factory("Graph", [ function () {
+app.factory("Graph", ["NxtD3Service", function (NxtD3Service) {
 
-  // The timeline
-  var svg;
+  var svg,
 
   // D3 components
-  var xScale; // The only d3 scale for placement on the x axis within the whole
+  xScale, // The only d3 scale for placement on the x axis within the whole
               // timeline.
-  var xAxis;
-  var yAxis;
-  var yScale;
-  var ordinalYScale; // Scale used to place events in lines for each type
+  xAxis,
+  yAxis,
+  yScale,
 
   // Interaction functions
-  var clicked;
-  var hoovered;
+  clicked,
+  hoovered,
 
-  // Timeline elements
-  var noDataIndicator;
-  var nowIndicator;
-  var brushg;
-  var circles; // events
-  var lines; // events start - end
-  var bars; // rain intensity
+  // Graph elements
+  nowIndicator;
 
   /**
    * @constructor
@@ -45,39 +38,38 @@ app.factory("Graph", [ function () {
    *                            an object containing top,
    *                            bottom, left and right padding.
    *                            All values in px.
-   * @param {integer} start begin value in milliseconds from epoch.
-   * @param {integer} end end value in milliseconds from epoch.
    * @param {object} interaction  optional object containing callback functions
    *  for click and hover interaction.
    */
-  function Graph(element, dimensions, start, end, interaction) {
+  function Graph(svg, dimensions, data, interaction) {
     this.dimensions = angular.copy(dimensions);
-    svg = createCanvas(element, this.dimensions);
+    svg = this._createCanvas(svg, this.dimensions);
     var width = dimensions.width -
                 dimensions.padding.left -
                 dimensions.padding.right;
-    xScale = makeScale({min: new Date(start), max: new Date(end)},
-                            {min: 0, max: width},
-                            { type: 'time' });
-    xAxis = makeAxis(xScale, {orientation: "bottom", ticks: 5});
-    drawAxes(svg, xAxis);
     if (interaction) {
       if (interaction.clickFn) {
-        clicked = setClickFunction(xScale, this.dimensions,
-          interaction.clickFn);
+        // clicked = setClickFunction(xScale, this.dimensions,
+        //   interaction.clickFn);
       }
       if (interaction.moveFn) {
-        brushed = setBrushFunction(xScale, interaction.brushFn);
+        // brushed = setMoveFunction(xScale, interaction.brushFn);
       }
     }
   }
 
-  Graph.prototype = {
+  Graph.prototype = Object.create(NxtD3Service.prototype, {
 
     constructor: Graph,
 
-    // TODO: create real noDataIndicator, this is just legacy code
-    addNoDataIndicator: function () {}
-  };
+  });
+
+  return Graph;
 
 }]);
+
+// xScale = this._makeScale({min: new Date(start), max: new Date(end)},
+    //                         {min: 0, max: width},
+    //                         { type: 'time' });
+    // xAxis = this._makeAxis(xScale, {orientation: "bottom", ticks: 5});
+    // drawAxes(svg, xAxis);
