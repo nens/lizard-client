e92f38c632d9acd0372f3072ed0f3bce17deb819
d-weeteling <Dirk.Weeteling@nelen-schuurmans.nl>
2014-09-05 12:22:41 +0200

    Extract logic to TemporalVectorService

--- source/app/directives/aggregate-directives.js
+++ source/app/directives/aggregate-directives.js
@@ -469,20 +469,10 @@ app.directive('temporalVectorLayer', ['UtilService', 'MapService', 'TemporalVect
   var API_URL = '/api/v1/tiles/location/5/16/10.geojson';
 
   // declaring local vars
-  var tvData,
-      mustDrawTVLayer,
-      setTVData,
-      createTVLayer,
-      updateTVLayer,
-      d3TVLayer,
-      getTimeIndex,
+  var d3TVLayer,
       previousTimeIndex,
-      changeTVData,
-      clearTVLayer;
-
-  // mustDrawTVLayer = function (scope) {
-  //   return scope.mapState.layers.flow.active;
-  // };
+      setTVData,
+      tvData;
 
   setTVData = function () {
 
@@ -503,101 +493,6 @@ app.directive('temporalVectorLayer', ['UtilService', 'MapService', 'TemporalVect
     request.send();
   };
 
-  // getTimeIndex = function (scope, tvData, stepSize) {
-
-  //   var virtualNow = scope.timeState.at,
-  //       relevantTimestamps = tvData.features[0].properties.timeseries[0].data,
-  //       currentTimestamp,
-  //       minTimestamp,
-  //       maxTimestamp;
-
-  //   if (relevantTimestamps.length === 0) {
-  //     console.log("[E] we don't have any relevant timestamps (i.e. no data!)");
-  //     return;
-  //   }
-
-  //   minTimestamp = relevantTimestamps[0];
-  //   maxTimestamp = relevantTimestamps[relevantTimestamps.length - 1];
-
-  //   if (virtualNow < minTimestamp) {
-
-  //     previousTimeIndex = 0;
-  //     TemporalVectorService.clearTVLayer();
-  //     return;
-
-  //   } else if ( virtualNow > maxTimestamp) {
-
-  //     TemporalVectorService.clearTVLayer();
-  //     return;
-
-  //   } else {
-
-  //     var i,
-  //         startIndex = previousTimeIndex || 0,
-  //         endIndex = relevantTimestamps.length - startIndex;
-
-  //     for (i = startIndex; i < endIndex; i++) {
-
-  //       currentTimestamp = relevantTimestamps[i];
-
-  //       if (currentTimestamp >= virtualNow
-  //           && currentTimestamp < virtualNow + stepSize) {
-
-  //         return i;
-  //       }
-  //     }
-  //   }
-  // };
-
-  // /**
-  //  * Creates svg layer in leaflet's overlaypane and adds current speed/direction
-  //  * as arrows
-  //  *
-  //  * @parameter {object} scope - A ng scope s.t. scope.map is defined
-  //  * @parameter {object} data - Object
-  //  * @return    {object} eventLayer - Leaflet layer object
-  //  */
-  // createTVLayer = function (scope, data) {
-
-  //   // if d3currentlayer does not exist atm, create it.
-  //   if (d3TVLayer === undefined) {
-  //     d3TVLayer = L.nonTiledGeoJSONd3(data, {
-  //       ext: 'd3',
-  //       name: 'current',
-  //       selectorPrefix: 'a',
-  //       class: 'current-arrow'
-  //     });
-  //   }
-
-  //   MapService.addLayer(d3TVLayer);
-
-  //   // for backwards compatibility.
-  //   d3TVLayer.g = d3TVLayer._container.selectAll("g");
-  //   d3TVLayer.reset = d3TVLayer._onMove;
-
-  //   return d3TVLayer;
-  // };
-
-  /**
-   * Updates svg layer in leaflet's overlaypane with new data object
-   *
-   * First call the reset function to give the svg enough space for the
-   * new data.Identify path elements with data objects via id and update,
-   * create or remove elements.
-   *
-   * @parameter {object} currentLayer - currentLayer object to update
-   * @parameter {object} data - data object
-   * @returns {void}
-   */
-  // updateTVLayer = function (tvLayer, data, timeIndex) {
-  //   tvLayer._data = data;
-  //   tvLayer._refreshDataForCurrents(timeIndex);
-  // };
-
-  // clearTVLayer = function () {
-  //   d3.selectAll("polygon.current-arrow").remove();
-  // };
-
   return {
     restrict: 'A',
     link: function (scope, element, attrs) {
@@ -613,7 +508,13 @@ app.directive('temporalVectorLayer', ['UtilService', 'MapService', 'TemporalVect
       getTimeIndexAndUpdate = function () {
 
         if (tvData && TemporalVectorService.mustDrawTVLayer(scope)) {
-          timeIndex = TemporalVectorService.getTimeIndex(scope, tvData, STEP_SIZE);
+
+          timeIndex = TemporalVectorService.getTimeIndex(
+            scope,
+            tvData,
+            STEP_SIZE
+          );
+
           if (timeIndex !== undefined) {
             previousTimeIndex = timeIndex;
             TemporalVectorService.updateTVLayer(
@@ -624,8 +525,6 @@ app.directive('temporalVectorLayer', ['UtilService', 'MapService', 'TemporalVect
           }
         }
       };
-
-      //var tvLayer;
 
       scope.$watch('timeState.at', function (newVal, oldVal) {
 
