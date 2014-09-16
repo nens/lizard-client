8e5e7881aa7624b568fd8c2f9f63ce4d26fbbfb1
fritzvd <f.vandeventer@gmail.com>
2014-09-16 15:23:42 +0200

    vector service

---
+++ source/app/services/vector-service.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @class VectorService
+ * @memberof app
+ * @name VectorService
+ * @summary Receives and returns vector data, as a service (or VDaaS).
+ * @description VectorService is responsible for retreiving, storing
+ * and exposing vector typed data.
+ *
+ */
+
+app.service('VectorService', ['Restangular',
+  function (Restangular) {
+
+    var getData, bla;
+
+    /**
+     * @memberof app.VectorService
+     * @function 
+     * @description gets data from backend
+     * @param  {string} layer layername
+     * @param  {object} geom  geometry that it needs to get (e.g. bboxs)
+     * @param  {object} time  start, stop object
+     * @return {promise} 
+     */
+    getData = function (layer, geom, time) {
+      if (time) {
+        start = time.start;
+        stop = time.stop;
+      }
+
+      var eventResource = Restangular.all('api/v1/events/');
+
+      return eventResource.get({
+        geom: geom,
+        start: start,
+        stop: stop,
+        object: layer.object_type + '$' +layer.object_pk
+      });
+    };
+
+    return {
+      getData: getData
+    }
+  }
+]);
---
+++ test/vector-service-tests.js
@@ -0,0 +1,31 @@
+describe('Testing VectorService', function () {
+  var $scope, $rootScope, VectorService, MapService;
+
+  beforeEach(module('lizard-nxt'));
+  beforeEach(inject(function ($injector, $compile) {
+    $rootScope = $injector.get('$rootScope');
+    VectorService = $injector.get('VectorService');
+    MapService = $injector.get('MapService');
+  }));
+
+  it('should retrieve data on call.', function () {
+    var elem = document.querySelector('body').appendChild(
+      document.createElement('div')
+      );
+    MapService.createMap(elem, {
+      bounds: window.data_bounds.all
+    });
+    MapService.initiateMapEvents();
+    var bounds = MapService.mapState.bounds;
+    var geom = "POLYGON(("
+      + bounds.getWest() + " " + bounds.getSouth() + ", "
+      + bounds.getEast() + " " + bounds.getSouth() + ", "
+      + bounds.getEast() + " " + bounds.getNorth() + ", "
+      + bounds.getWest() + " " + bounds.getNorth() + ", "
+      + bounds.getWest() + " " + bounds.getSouth()
+      + "))";
+    VectorService.getData('jean', geom);
+    expect(VectorService.dataStore).toBe(undefined);
+  });
+
+});
