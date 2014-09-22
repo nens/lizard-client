6e36722f52959999e414d0e294fd221119c9563a
fritzvd <f.vandeventer@gmail.com>
2014-09-22 17:04:25 +0200

    vector service has filters and saves data

--- source/app/services/vector-service.js
+++ source/app/services/vector-service.js
@@ -11,10 +11,82 @@
  *
  */
 
-app.service('VectorService', ['Restangular',
-  function (Restangular) {
+app.service('VectorService', ['Restangular', 'LeafletService',
+  function (Restangular, LeafletService) {
 
-    var getData, bla;
+    /**
+     * @function
+     * @memberOf app.VectorService
+     * @description filters geojson array on spatial bounds.
+     * @param  {L.LatLngBounds} spatial
+     * @param  {featureArray}   sourceArray
+     * @return {filteredSet}    filtered set of features.
+     */
+    var filterSpatial = function (sourceArray, spatial) {
+      var filteredSet = [];
+      sourceArray.forEach(function (feature) {
+        var withinBounds = spatial.contains(new LeafletService.LatLng(feature.geometry.coordinates[0], feature.geometry.coordinates[1]));
+        if (withinBounds) {
+          filteredSet.push(feature);
+        }
+      });
+      return filteredSet;
+    };
+
+    /**
+     * @function
+     * @memberOf app.VectorService
+     * @description filters geojson array on temporal bounds.
+     * @param  {object}         start end object
+     * @param  {featureArray}   sourceArray
+     * @return {filteredSet}    filtered set of features.
+     */
+    var filterTemporal = function (sourceArray, temporal) {
+      var filteredSet = [];
+      sourceArray.forEach(function (feature) {
+        var afterStart = false;
+        var beforeEnd = false;
+        if (temporal.start) {
+          afterStart = feature.properties.timestamp_start >= temporal.start;
+        }
+        if (temporal.end) {
+          beforeEnd = feature.properties.timestamp_end <= temporal.end;
+        }
+
+        if ((afterStart || temporal.start === undefined) &&
+            (beforeEnd || temporal.end === undefined)) {
+          filteredSet.push(feature);
+        }
+      });
+      return filteredSet;
+    };
+
+    /**
+     * @description filters data on time and spatial extent
+     * @param  {L.LatLngBounds} spatial  Leaflet Bounds object
+     * @param  {object}         temporal object with start and end in epoch timestamp
+     * @return {filteredSet}    Array with points within extent.
+     */
+    var filterSet = function (sourceArray, spatial, temporal) {
+      if (!spatial && !temporal) { return sourceArray; }
+
+      var filteredSet = [];
+      if (spatial instanceof LeafletService.LatLngBounds) {
+        filteredSet = filterSpatial(sourceArray, spatial);
+      } else {
+        filteredSet = sourceArray;
+      }
+
+      if (!temporal) { return filteredSet; }
+
+      if (temporal.hasOwnProperty('start') || temporal.hasOwnProperty('end')) {
+        filteredSet = filterTemporal(filteredSet, temporal);
+      }
+
+      return filteredSet;
+    };
+
+    var vectorLayers = {};
 
     /**
      * @memberof app.VectorService
@@ -25,26 +97,26 @@ app.service('VectorService', ['Restangular',
      * @param  {object} time  start, stop object
      * @return {promise} 
      */
-    getData = function (layer, geom, time) {
-      var start, stop;
-      
-      if (time) {
-        start = time.start;
-        stop = time.stop;
+    var getData = function (layer, geomortime, time) {
+      // if only one extra argument it can be geom or time.
+      if (!time && !(geomortime instanceof L.LatLngBounds)) {
+        return filterSet(vectorLayers[layer], undefined, geomortime);
       }
+      return filterSet(vectorLayers[layer], geomortime, time);
+    };
 
-      var eventResource = Restangular.all('api/v1/events/');
-
-      return eventResource.get({
-        geom: geom,
-        start: start,
-        stop: stop,
-        object: layer.object_type + '$' + layer.object_pk
-      });
+    var setData = function (layer, data) {
+      if (vectorLayers.hasOwnProperty(layer)) {
+        vectorLayers[layer].data = data;
+      } else {
+        vectorLayers[layer] = data;
+      }
     };
 
     return {
-      getData: getData
-    }
+      getData: getData,
+      setData: setData,
+      layers: vectorLayers
+    };
   }
 ]);
