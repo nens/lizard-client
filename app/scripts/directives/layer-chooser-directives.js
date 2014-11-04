1900bbf2f5e8116c845eb1c4cb8cc4fb6a489ce3
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2014-11-04 14:49:27 +0100

    quik and dirty layer-chooser last in line

--- app/scripts/directives/layer-chooser-directives.js
+++ app/scripts/directives/layer-chooser-directives.js
@@ -18,35 +18,36 @@ angular.module('lizard-nxt')
       }
     });
 
-    // Speed-up initial load of the app, this is put in a timeout so
-    // the real map is handled first.
-    setTimeout(function () {
-      var chooser = new NxtMap(element.find('.layer-img')[0], [layerGroup], {
-        center: [52.39240447569775, 5.101776123046875],
-        zoom: 6,
-        dragging: false,
-        touchZoom: false,
-        doubleClickZoom: false,
-        tap: false,
-        scrollWheelZoom: false,
-        animate: true,
-        zoomControl: false,
-        attributionControl: false,
-        forChooser: true
-      });
+    var chooser = new NxtMap(element.find('.layer-img')[0], [layerGroup], {
+      center: [52.39240447569775, 5.101776123046875],
+      zoom: 6,
+      dragging: false,
+      touchZoom: false,
+      doubleClickZoom: false,
+      tap: false,
+      scrollWheelZoom: false,
+      animate: true,
+      zoomControl: false,
+      attributionControl: false,
+      forChooser: true
+    });
 
+
+    // To speed-up initial load of the app layerchooser layers are toggled
+    // after 3000 ms.
+    setTimeout(function () {
+      console.log('turn layer chooser on');
       chooser.toggleLayerGroup(
         chooser.layerGroups[Object.keys(chooser.layerGroups)[0]]
       );
+    }, 3000);
 
-      scope.$watch('mapState.bounds', function (n, v) {
-        if (n === v) { return; }
-        var zoom = scope.mapState.zoom;
-        var centroid = scope.mapState.bounds.getCenter();
-        chooser.setView(centroid, zoom - 2);
-      });
-
-    }, 1000);
+    scope.$watch('mapState.bounds', function (n, v) {
+      if (n === v) { return; }
+      var zoom = scope.mapState.zoom;
+      var centroid = scope.mapState.bounds.getCenter();
+      chooser.setView(centroid, zoom - 2);
+    });
 
   };
 
