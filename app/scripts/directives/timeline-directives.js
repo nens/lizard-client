dce483a9e5f9c82a258c2ee577b9a741f02bf9f7
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2014-11-19 21:39:38 +0100

    fix bouncing around of time

--- app/scripts/directives/timeline-directives.js
+++ app/scripts/directives/timeline-directives.js
@@ -69,9 +69,6 @@ angular.module('lizard-nxt')
         scope.$apply(function () {
           scope.timeState.resolution = (
             scope.timeState.end - scope.timeState.start) /  window.innerWidth;
-          // snap timeState.at to nearest interval
-          scope.timeState.at = UtilService.roundTimestamp(
-            scope.timeState.at, scope.timeState.aggWindow, false);
           getTimeLineData();
         });
       },
@@ -87,12 +84,13 @@ angular.module('lizard-nxt')
        * @param {object} dimensions - object with timeline dimensions.
        */
       clickFn: function (event, scale, dimensions) {
-        var timeClicked = +(scale.invert(event.pageX - dimensions.padding.left));
+        var timeClicked = +(scale.invert(event.x - dimensions.padding.left));
         scope.timeState.at = UtilService.roundTimestamp(
           timeClicked,
           scope.timeState.aggWindow,
           false
         );
+
         scope.$digest();
       },
     };
@@ -103,7 +101,6 @@ angular.module('lizard-nxt')
     // Initialise timeline
     var timeline = new Timeline(
       el[0], dimensions, start, end, interaction);
-
 
     // HELPER FUNCTIONS
 
