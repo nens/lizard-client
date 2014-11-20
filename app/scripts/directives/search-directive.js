bab50b38dc43398c405cdf9a5a81eab52cb09288
fritzvd <f.vandeventer@gmail.com>
2014-11-20 16:24:21 +0100

    docs for directive

--- app/scripts/directives/search-directive.js
+++ app/scripts/directives/search-directive.js
@@ -1,9 +1,20 @@
+'use strict';
+
+/**
+ * @description directive that displays search
+ * and makes sure the right services are called.
+ */
 angular.module('lizard-nxt')
   .directive('search', ['LocationService', function (LocationService) {
     
   var link = function (scope, element, attrs) {
     
-    // 13 refers to the RETURN key.
+    /**
+     * @description event handler for key presses.
+     * checks if enter is pressed, does search.
+     * @param {event} event that is fired. 
+     * 13 refers to the RETURN key.
+     */
     scope.searchKeyPress = function ($event) {
       if ($event.target.id === "searchboxinput" &&
           $event.which === 13) {
@@ -12,6 +23,10 @@ angular.module('lizard-nxt')
       element.focus();
     };
 
+    /**
+     * @description calls LocationService
+     * with the right query and puts in on the scope.
+     */
     scope.search = function () {
       LocationService.search(scope.geoquery)
           .then(function (response) {
@@ -21,14 +36,26 @@ angular.module('lizard-nxt')
         });
     };
 
+    /**
+     * @description removes location model from box content
+     */
     var destroyLocationModel = function () {
       delete scope.box.content.location;
     };
     
+    /**
+     * @description resets input field
+     * on scope, because also needs to trigger on reset button,
+     * not just on succesful search/zoom.
+     */
     scope.cleanInput = function () {
       scope.geoquery = "";
     };
    
+    /**
+     * @description zooms to search result
+     * @param {object} one search result.
+     */
     scope.zoomTo = function (obj) {
       if (obj.boundingbox) {
         var southWest = new L.LatLng(obj.boundingbox[0], obj.boundingbox[2]);
