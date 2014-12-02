82cf4a55fc65b778bc8e87256c08ed09187152e1
fritzvd <f.vandeventer@gmail.com>
2014-12-02 14:02:54 +0100

    changes to sassing

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,8 +56,8 @@ module.exports = function (grunt) {
         tasks: ['newer:jshint', 'karma:dev']
       },
       styles: {
-        files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
-        tasks: ['newer:copy:styles', 'autoprefixer']
+        files: ['<%= yeoman.app %>/styles/{,*/}*.{scss}'],
+        tasks: ['newer:sass:watch', 'newer:copy:styles', 'autoprefixer']
       },
       gruntfile: {
         files: ['Gruntfile.js']
@@ -244,6 +244,27 @@ module.exports = function (grunt) {
       }
     },
 
+    // Node Sass is faster than Compass
+    sass: {
+      options: {
+        sourceMap: true,
+        outputStyle: 'expanded'
+      },
+      dist: {
+        files: {
+          '.tmp/styles/main.css': '<%=yeoman.app %>/styles/main.scss'
+        },
+        options: {
+          outputStyle: 'compressed'
+        }
+      },
+      watch: {
+        files: {
+          '.tmp/styles/main.css': '<%= yeoman.app %>/styles/main.scss'
+        }
+      }
+    },
+    
     // Renames files for browser caching purposes
     filerev: {
       dist: {
@@ -475,6 +496,7 @@ module.exports = function (grunt) {
       'wiredep',
       'configureProxies',
       'concurrent:server',
+      'sass:watch',
       'autoprefixer',
       'connect:livereload',
       'watch'
@@ -501,6 +523,7 @@ module.exports = function (grunt) {
     'wiredep',
     'useminPrepare',
     'html2js',
+    'sass',
     'concurrent:dist',
     'autoprefixer',
     'concat',
