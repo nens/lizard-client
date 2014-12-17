a0714d9dd8a440813aae5b693188b13927b61ec9
fritzvd <f.vandeventer@gmail.com>
2014-12-17 14:49:26 +0100

    fix csv download ... add sanitize, csv

--- app/components/omnibox/controllers/rain-controller.js
+++ app/components/omnibox/controllers/rain-controller.js
@@ -1,5 +1,7 @@
 angular.module('lizard-nxt')
-  .controller("rain", ['$scope', function ($scope) {
+  .controller("rain",
+      ['RasterService', 'State', '$scope',
+  function (RasterService, State, $scope) {
 
   /*
    * @description
@@ -11,18 +13,27 @@ angular.module('lizard-nxt')
     $scope.fullDetails = n;
   });
 
+  var getRecurrenceTime = function () {
+    var data = [{"max": 28.139999894425273, "end": 1418413200000.0, "t": "T <= 1", "td_window": "2 dag(en)", "start": 1418240400000.0}, {"max": 22.94999991171062, "end": 1418386800000.0, "t": "T <= 1", "td_window": "1 dag(en)", "start": 1418300400000.0}, {"max": 8.579999996349216, "end": 1418349600000.0, "t": "T <= 1", "td_window": "3 uur", "start": 1418338800000.0}, {"max": 3.800000010058284, "end": 1418349300000.0, "t": "T <= 1", "td_window": "1 uur", "start": 1418345700000.0}];
+    
+    RasterService.getData({
+      agg: 'rrc',
+      start: 1 
+    });
+    return data;
+  };
+
   /**
    * Format the CSV (exporting rain data for a point in space/interval in
    * time) in a way that makes it comprehensible for les autres.
    *
    */
   $scope.formatCSVColumns = function (data) {
-
     var i,
         formattedDateTime,
         formattedData = [],
-        lat = $scope.mapState.here.lat,
-        lng = $scope.mapState.here.lng,
+        lat = State.spatial.here.lat,
+        lng = State.spatial.here.lng,
         _formatDate = function (epoch) {
           var d = new Date(parseInt(epoch));
           return [
