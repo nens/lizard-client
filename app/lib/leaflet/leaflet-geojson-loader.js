907482fadba2c8c53fc55df701048569e71aee9d
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2014-12-18 10:01:11 +0100

    inital commit loader

---
+++ app/lib/leaflet/leaflet-geojson-loader.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name LeafletVectorService
+ * @description
+ * # LeafletVector
+ * Creates a Tiled Layer for retrieving and drawing vector data.
+ */
+angular.module('lizard-nxt')
+.service('LeafletGeoJsonLoader', ["LeafletService", "VectorService", "UtilService",
+  function (LeafletService, VectorService, UtilService) {
+
+  }
+]);
