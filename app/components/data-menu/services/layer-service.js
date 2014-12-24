808932c496de2c4dda2432c34f6387efd1f04eb0
Ernst Kuilder <ernst.kuilder@nelen-schuurmans.nl>
2014-12-24 14:05:25 +0100

    Update layer-service.js

--- app/components/data-menu/services/layer-service.js
+++ app/components/data-menu/services/layer-service.js
@@ -242,6 +242,9 @@ angular.module('lizard-nxt')
           };
 
           options = options || {};
+          
+          // Pass layer options to the services making the request.
+          // RasterServices uses this to add options.styles.
           angular.extend(options, this.options);
           options.agg = this.aggregationType;
 
