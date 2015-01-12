36fbbe226c6efdf7f884cb06a2b6988c0184f5f7
d-weeteling <Dirk.Weeteling@nelen-schuurmans.nl>
2015-01-12 16:10:35 +0100

    Added relevant template + directive 'baselayer-chooser-...';  WIP

---
+++ app/components/layer-chooser/baselayer-chooser-directives.js
@@ -0,0 +1,25 @@
+//layer-directive.js
+
+angular.module('data-menu')
+.directive("baselayerChooser", ['State', 'LeafletService',
+function (State, LeafletService)
+{
+
+  var link = function (scope, element, attrs) {
+    console.log("[!] Linking start...");
+
+    // ...
+    var allBaselayers = State.layerGroups;
+
+    console.log("allBaselayers =", allBaselayers);
+
+    console.log("[!] Linking finished.");
+  };
+
+  return {
+    link: link,
+    templateUrl: 'layer-chooser/baselayer-chooser.html',
+    restrict: 'E',
+  };
+
+}]);
