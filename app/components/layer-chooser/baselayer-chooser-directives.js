7500598f147e69923e9c6ea4de5a83959b14a9e0
d-weeteling <Dirk.Weeteling@nelen-schuurmans.nl>
2015-01-13 11:48:20 +0100

    Baselayergroups now swappable via single button

--- app/components/layer-chooser/baselayer-chooser-directives.js
+++ app/components/layer-chooser/baselayer-chooser-directives.js
@@ -1,26 +1,25 @@
 //layer-directive.js
 
 angular.module('data-menu')
-.directive("baselayerChooser", [
-  'State',
-  'LeafletService',
-  'DataService',
-
-function (
-  State,
-  LeafletService,
-  DataService
-)
+.directive("baselayerChooser", ['DataService', function (DataService)
 {
-
   var link = function (scope, element, attrs) {
-    console.log("[!] Linking start...");
 
-    // ...
-    var allBaselayers = DataService.baselayerGroups;
+    window.onload = function () {
 
-    console.log("allBaselayers =", allBaselayers);
-    console.log("[!] Linking finished.");
+      var _allBLGs = DataService.baselayerGroups,
+          _allBLGSlugs = _.pluck(_allBLGs, "slug"),
+          _getActiveBLG = function () {
+            return _.filter(_allBLGs, function (blg) {
+              return blg.isActive();
+            })[0];
+          };
+
+      scope.getNextInactiveBLG = function () {
+        var activeBLGIndex = _allBLGSlugs.indexOf(_getActiveBLG().slug);
+        return _allBLGs[(activeBLGIndex + 1) % _allBLGs.length];
+      };
+    };
   };
 
   return {
