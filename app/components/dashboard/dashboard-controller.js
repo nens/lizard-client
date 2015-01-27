11093dd4a8ee7e73d9f58b19fb0ac2c92db40bcc
fritzvd <f.vandeventer@gmail.com>
2015-01-27 14:20:44 +0100

    fire request, draw the tings

--- app/components/dashboard/dashboard-controller.js
+++ app/components/dashboard/dashboard-controller.js
@@ -3,6 +3,26 @@
  * Directive for dashboard component.
  */
 angular.module('dashboard')
-  .controller('DashboardCtrl', function () {
-    this.elements = window.dashboard;
-});  
+  .controller('DashboardCtrl', ['Restangular', 'DataService',
+      function (Restangular, DataService) {
+
+    
+    this.DataService = DataService;
+    this.elements = []; 
+
+    var that = this;
+
+    Restangular.one('api/v1/dashboards/1/').get()
+      .then(function (dashboard) {
+        that.elements = dashboard.dashboardelements;
+      });
+
+    this.elements.forEach(function (el, i) {
+      DataService[el.data].getData({
+        geom: 
+      })
+        .then(function (response) {
+          el.data = response;
+        });
+    });
+}]);
