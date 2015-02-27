637fc113e54c75ea108692725746c18203a3c052
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2015-02-27 16:25:51 +0100

    connect timeline height to context graph space

--- app/components/time-ctx/time-ctx-directive.js
+++ app/components/time-ctx/time-ctx-directive.js
@@ -1,10 +1,9 @@
 
 angular.module('time-ctx')
   .directive('timeCtx',
-             ["EventAggregateService", "State", "DataService",
-              function (EventAggregateService, State, DataService) {
+             ["EventAggregateService", "State", "DataService", "UtilService", "Timeline",
+              function (EventAggregateService, State, DataService, UtilService, Timeline) {
 
-  // draw full screen graph
   var link = function (scope, element, attrs) {
 
     var getWidth = function () {
@@ -12,73 +11,73 @@ angular.module('time-ctx')
     };
 
     var getHeight = function () {
-      return element.height();
+      return element.height() - 50; // min-height from top row
     };
 
-    scope.dimensions.width = getWidth() - 10;
-
-    var aggregateEvents = function () {
-      var eventAgg;
-      // reset eventAggs
-      scope.eventAggs = [];
-      angular.forEach(DataService.layerGroups, function (lg) {
-        lg.getData({
-          geom: State.spatial.bounds,
-          start: State.temporal.start,
-          end: State.temporal.end,
-          type: 'Event'
-        }).then(null, null, function (response) {
-
-          if (response && response.data) {
-            // aggregate response
-            eventAgg = {
-              data: EventAggregateService.aggregate(
-                      response.data,
-                      State.temporal.aggWindow,
-                      lg.mapLayers[0].color
-                    ),
-              ylabel: lg.name,
-              baseColor: lg.mapLayers[0].color
-            };
-
-            scope.eventAggs.push(eventAgg);
-            // calculate new dimensions
-            scope.dimensions.height =
-              (getHeight() / scope.eventAggs.length) - 20;
-          }
-        });
-      });
+    Timeline.onresize = function (dimensions) {
+      console.log(dimensions);
+      scope.tctx.dims.height = getHeight() - dimensions.height - 5; // 5 margin.
     };
 
-    /**
-     * Updates time-ctx when user pans or zooms map.
-     */
-    scope.$watch(State.toString('spatial.bounds'), function (n, o) {
-      if (n === o) { return true; }
-      aggregateEvents();
-    });
+    var nGraphs = 1;
 
-    /**
-     * Updates time-ctx when layers are added or removed.
-     */
-    scope.$watch(State.toString('layerGroups.active'), function (n, o) {
-      if (n === o) { return true; }
-      aggregateEvents();
-    });
+    scope.tctx.dims = {
+      width: UtilService.getCurrentWidth() + UtilService.TIMELINE_LEFT_MARGIN,
+      height: getHeight() / nGraphs,
+      padding: {
+        top: 5,
+        right: 0,
+        bottom: 0,
+        left: UtilService.TIMELINE_LEFT_MARGIN
+      }
+    };
+
+    // var aggregateEvents = function () {
+    //   var eventAgg;
+    //   // reset eventAggs
+    //   scope.eventAggs = [];
+    //   angular.forEach(DataService.layerGroups, function (lg) {
+    //     lg.getData({
+    //       geom: State.spatial.bounds,
+    //       start: State.temporal.start,
+    //       end: State.temporal.end,
+    //       type: 'Event'
+    //     }).then(null, null, function (response) {
+
+    //       if (response && response.data) {
+    //         // aggregate response
+    //         eventAgg = {
+    //           data: EventAggregateService.aggregate(
+    //                   response.data,
+    //                   State.temporal.aggWindow,
+    //                   lg.mapLayers[0].color
+    //                 ),
+    //           ylabel: lg.name,
+    //           baseColor: lg.mapLayers[0].color
+    //         };
+
+    //         scope.eventAggs.push(eventAgg);
+    //         // calculate new dimensions
+    //         scope.dimensions.height =
+    //           (getHeight() / scope.eventAggs.length) - 20;
+    //       }
+    //     });
+    //   });
+    // };
 
     /**
      * Updates time-ctx when time zoom changes.
      */
     scope.$watch(State.toString('temporal.timelineMoving'), function (n, o) {
       if (n === o) { return true; }
-      aggregateEvents();
+      // aggregateEvents();
     });
 
     // init
-    aggregateEvents();
+    // aggregateEvents();
 
     // hack to get color map for legend
-    scope.getColorMap = EventAggregateService.getColorMap;
+    // scope.getColorMap = EventAggregateService.getColorMap;
   };
 
   return {
