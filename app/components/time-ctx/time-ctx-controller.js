a666d46e981bed31503ae59185b81b40b1d5a39d
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2015-03-02 10:24:43 +0100

    create graph per data element

--- app/components/time-ctx/time-ctx-controller.js
+++ app/components/time-ctx/time-ctx-controller.js
@@ -1,16 +1,18 @@
 angular.module('time-ctx')
   .controller("TimeCtxCtrl", ["$scope", "State", function ($scope, State) {
 
-  this.eventAggs = undefined;
-
   this.state = State;
 
   // dimensions are dependent on screen size.
   // this is calculated in directive.
-  this.dimens = {};
+  this.dims = {};
+
+  this.content = {};
 
   // statistics (maybe get dynamically from event aggregation service?)
   this.stats = ['max', 'min', 'mean', 'sum', 'median', 'count'];
+
+  this.eventAggs = undefined;
 
   // default selection
   this.selectedStat = this.selectedStat || this.stats[2];
