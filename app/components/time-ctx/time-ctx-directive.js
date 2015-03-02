85c2e6785a4df9f0037518ceb07320c4eeef4e60
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2015-03-02 14:35:32 +0100

    give little bit of margin and use dataservice for data

--- app/components/time-ctx/time-ctx-directive.js
+++ app/components/time-ctx/time-ctx-directive.js
@@ -8,6 +8,7 @@ angular.module('time-ctx')
 
     var TL_TOP_MARGIN = 10,
         GRAPH_PADDING = 5,
+        tlDims = {},
         nGraphs = 1;
 
 
@@ -20,66 +21,117 @@ angular.module('time-ctx')
       return element.height() - 50; // min-height from top row
     };
 
-    Timeline.onresize = function (dimensions) {
+    var resize = function (tlDimensions) {
+      tlDims = tlDimensions;
       nGraphs = Object.keys(scope.tctx.content).length;
-
-      scope.tctx.dims.height = (getHeight() - dimensions.height - TL_TOP_MARGIN) / nGraphs  - GRAPH_PADDING;
+      scope.tctx.dims.height =
+        (getHeight() - tlDimensions.height - TL_TOP_MARGIN) / nGraphs;
     };
 
+    Timeline.onresize = resize;
+
     scope.tctx.dims = {
-      width: UtilService.getCurrentWidth() + UtilService.TIMELINE_LEFT_MARGIN,
+      width: UtilService.getCurrentWidth() + 0.83 * UtilService.TIMELINE_LEFT_MARGIN,
       height: getHeight() / nGraphs,
       padding: {
         top: GRAPH_PADDING,
         right: 0,
-        bottom: 0,
-        left: UtilService.TIMELINE_LEFT_MARGIN
+        bottom: 2 * GRAPH_PADDING, // Enough for the line of the axis.
+        left: 0.83 * UtilService.TIMELINE_LEFT_MARGIN
       }
     };
 
-    scope.tctx.content = [];
+    var geom = State.box.type === 'area'
+      ? State.spatial.bounds
+      : State.box.type === 'line'
+        ? State.spatial.points
+        : State.spatial.here;
 
-    for (var i = 15 - 1; i >= 0; i--) {
-      scope.tctx.content[i] = {
-        data: []
-      };
-      scope.tctx.content[i].data = [[2, 0], [0, 1], [4, 3], [7, 0], [8, 1], [9, 3], [10, 0], [11, 1], [15, 3]];
-    }
+    var getTimeData = function () {
+      DataService.getData('time', {
+        geom: geom,
+        start: State.temporal.start,
+        end: State.temporal.end,
+        temporalOnly: true
+      }).then(null, null, function (response) {
+
+
+        scope.tctx.content[response.layerSlug] = {};
+        scope.tctx.content[response.layerSlug].data = response.data;
+
+        if (UtilService.isSufficientlyRichData(
+          (response.data && response.data.data) || response.data
+          )) {
+
+          var sharedKeys = [
+            'format',
+            'data',
+            'scale',
+            'quantity',
+            'unit',
+            'color',
+            'type'
+          ];
+
+          angular.forEach(sharedKeys, function (key) {
+            scope.tctx.content[response.layerSlug][key] = response[key];
+          });
+
+
+          if (tlDims) {
+            resize(tlDims);
+          }
+        }
+
+      });
+    };
+
+    getTimeData();
+
+
+    // scope.tctx.content = [];
+
+    // for (var i = 15 - 1; i >= 0; i--) {
+    //   scope.tctx.content[i] = {
+    //     data: []
+    //   };
+    //   scope.tctx.content[i].data = [[2, 0], [0, 1], [4, 3], [7, 0], [8, 1], [9, 3], [10, 0], [11, 1], [15, 3]];
+    // }
 
 
 
-    // var aggregateEvents = function () {
-    //   var eventAgg;
-    //   // reset eventAggs
-    //   scope.eventAggs = [];
-    //   angular.forEach(DataService.layerGroups, function (lg) {
-    //     lg.getData({
-    //       geom: State.spatial.bounds,
-    //       start: State.temporal.start,
-    //       end: State.temporal.end,
-    //       type: 'Event'
-    //     }).then(null, null, function (response) {
+    // // var aggregateEvents = function () {
+    // //   var eventAgg;
+    // //   // reset eventAggs
+    // //   scope.eventAggs = [];
+    // //   angular.forEach(DataService.layerGroups, function (lg) {
+    // //     lg.getData({
+    // //       geom: State.spatial.bounds,
+    // //       start: State.temporal.start,
+    // //       end: State.temporal.end,
+    // //       type: 'Event'
+    // //     }).then(null, null, function (response) {
 
-    //       if (response && response.data) {
-    //         // aggregate response
-    //         eventAgg = {
-    //           data: EventAggregateService.aggregate(
-    //                   response.data,
-    //                   State.temporal.aggWindow,
-    //                   lg.mapLayers[0].color
-    //                 ),
-    //           ylabel: lg.name,
-    //           baseColor: lg.mapLayers[0].color
-    //         };
+    // //       if (response && response.data) {
+    // //         // aggregate response
+    // //         eventAgg = {
+    // //           data: EventAggregateService.aggregate(
+    // //                   response.data,
+    // //                   State.temporal.aggWindow,
+    // //                   lg.mapLayers[0].color
+    // //                 ),
+    // //           ylabel: lg.name,
+    // //           baseColor: lg.mapLayers[0].color
+    // //         };
 
-    //         scope.eventAggs.push(eventAgg);
-    //         // calculate new dimensions
-    //         scope.dimensions.height =
-    //           (getHeight() / scope.eventAggs.length) - 20;
-    //       }
-    //     });
-    //   });
-    // };
+    // //         scope.eventAggs.push(eventAgg);
+    // //         // calculate new dimensions
+    // //         scope.dimensions.height =
+    // //           (getHeight() / scope.eventAggs.length) - 20;
+    // //       }
+    // //     });
+    // //   });
+    // // };
 
     /**
      * Updates time-ctx when time zoom changes.
