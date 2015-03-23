2266b877f03ca56034fa51766bf7a16b09859893
Arjen Vrielink <arjen.vrielink@gmail.com>
2015-03-23 15:17:31 +0100

    Temp fix for not updating omnibox; get rid of duplicate function

--- app/components/data-menu/data-menu-controller.js
+++ app/components/data-menu/data-menu-controller.js
@@ -14,32 +14,50 @@
  */
 angular.module('data-menu')
   .controller('DatamenuController',
-              ['$scope', 'DataService', 'State', 'MapService',
-               function ($scope, DataService, State, MapService) {
+              ['$scope', 'DataService', 'State', 'MapService', '$timeout',
+               function ($scope, DataService, State, MapService, $timeout) {
 
-      this.layerGroups = DataService.layerGroups;
+    this.layerGroups = DataService.layerGroups;
 
-      this.toggleLayerGroup = DataService.toggleLayerGroup;
+    this.toggleLayerGroup = DataService.toggleLayerGroup;
 
-      this.box = State.box;
+    this.box = State.box;
 
-      this.enabled = false;
+    this.enabled = false;
 
-      this.state = State.layerGroups;
+    this.state = State.layerGroups;
 
-      // move this function to service.
-      this.zoomToBounds = function (spatialBounds, temporalBounds) {
+    // move this function to service.
+    this.zoomToBounds = function (spatialBounds, temporalBounds) {
 
-        // zoom to spatial bounds
-        MapService.fitBounds(spatialBounds);
+    /**
+     * Set timeline to moving and back after digest loop to trigger watches
+     * that do something after the timeline moved.
+     */
+    var announceMovedTimeline = function () {
+      State.temporal.timelineMoving = true;
 
-        // zoom to temporal bounds
-        if (temporalBounds.start !== temporalBounds.end) {
-          State.temporal.start = temporalBounds.start;
-          State.temporal.end = temporalBounds.end;
-          State.temporal.at = temporalBounds.start;
-        }
-      };
+      // Set timeline moving to false after digest loop
+      $timeout(
+        function () {
+          State.temporal.timelineMoving = false;
+        },
+        0, // no delay, fire when digest ends
+        true // trigger new digest loop
+      );
+    };
+
+    // zoom to spatial bounds
+    MapService.fitBounds(spatialBounds);
+
+    // zoom to temporal bounds
+    if (temporalBounds.start !== temporalBounds.end) {
+      State.temporal.start = temporalBounds.start;
+      State.temporal.end = temporalBounds.end;
+      State.temporal.at = temporalBounds.start;
+      announceMovedTimeline();
     }
-  ]);
+  };
+  }
+]);
 
