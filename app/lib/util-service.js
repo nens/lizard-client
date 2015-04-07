d9e914bdd56fe39370ccc64223145288558272be
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2015-04-07 15:21:34 +0200

    limit future to 24 hours

--- app/lib/util-service.js
+++ app/lib/util-service.js
@@ -626,7 +626,7 @@ angular.module('lizard-nxt')
   })();
 
   this.MIN_TIME = (new Date("Jan 01, 1970")).getTime();
-  this.MAX_TIME = (new Date()).getTime() + 3 * 60 * 60 * 1000;
+  this.MAX_TIME = (new Date()).getTime() + 24 * 60 * 60 * 1000;
   this.TIMELINE_LEFT_MARGIN = 60;
   this.TIMELINE_RIGHT_MARGIN = 40;
 
