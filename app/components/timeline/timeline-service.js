ec476f5b1fddfa1e10787564cd0b469f3a55962e
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2015-04-07 15:21:22 +0200

    fix https://github.com/nens/lizard-nxt/issues/823, limit zooming in timeline-service

--- app/components/timeline/timeline-service.js
+++ app/components/timeline/timeline-service.js
@@ -107,7 +107,7 @@ angular.module('lizard-nxt')
           .attr("width", width)
           .attr('title', 'Het gedeelte van de tijdlijn dat in de toekomst ligt')
           .attr("id", "nodata")
-          .attr("x", function () {return xScale(Date.now()); })
+          .attr("x", xScale(Date.now()))
           .attr("opacity", 0.8)
           .style("fill", "#DDD");
       }
@@ -567,6 +567,22 @@ angular.module('lizard-nxt')
     var zoomed = function () {
       d3.event.sourceEvent.preventDefault();
 
+      var ONE_HOUR = 1000 * 60 * 60;
+
+      var start = UtilService.getMinTime(xScale.domain()[0].getTime()),
+          end = UtilService.getMaxTime(xScale.domain()[1].getTime());
+
+      // Min domain when zooming is ONE_HOUR
+      if (start === UtilService.MIN_TIME && end !== UtilService.MAX_TIME) {
+        end = end >= start + ONE_HOUR  ? end : start + ONE_HOUR;
+      }
+
+      else if (end === UtilService.MAX_TIME && start !== UtilService.MIN_TIME) {
+        start = start <= end - ONE_HOUR ? start : end - ONE_HOUR;
+      }
+
+      xScale.domain([start, end]);
+
       drawTimelineAxes(svg, xScale, dimensions);
 
       if (bars) {
@@ -581,10 +597,9 @@ angular.module('lizard-nxt')
       }
 
       if (futureIndicator) {
+        var width = Timeline.prototype._getWidth(dimensions);
         futureIndicator
-          .attr('x', function () {
-            return xScale(Date.now());
-          });
+          .attr('x', xScale(Date.now()));
       }
 
       if (circles) {
@@ -706,12 +721,11 @@ angular.module('lizard-nxt')
     dimensions
     ) {
 
-    var height = Timeline.prototype._getHeight(dimensions);
+    var height = Timeline.prototype._getHeight(dimensions),
+        width = Timeline.prototype._getWidth(dimensions);
 
     futureIndicator
-     .attr('x', function () {
-        return xScale(Date.now());
-      });
+      .attr('x', xScale(Date.now()));
 
     if (dimensions.height < oldDimensions.height) {
       futureIndicator
