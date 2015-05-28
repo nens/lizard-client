nens/lizard-client

commit eef810b75da6a89939ad6fc55cd4724120090624

fix https://github.com/nens/lizard-nxt/issues/863 set time when adding temporal layer

app/components/map/services/map-service.js
@@ -194,6 +194,13 @@ angular.module('map')
 
       _toggleLayers: function (lg) {
         if (lg.isActive() && lg.mapLayers.length > 0) {
+          if (lg.temporal) {
+            // copy timeState to layers so when added they will respect the
+            // current timestate.
+            angular.forEach(lg.mapLayers, function (layer) {
+              layer.timeState = State.temporal;
+            });
+          }
           addLayersRecursively(service._map, lg.mapLayers, 0);
         }
         else {
@@ -255,7 +262,7 @@ angular.module('map')
        */
       initializeLayers: function (timeState) {
         angular.forEach(DataService.layerGroups, function (lg, lgSlug) {
-          this.initializeLayer(lg, timeState);
+          this.initializeLayer(lg);
         }, this);
       },
 
@@ -268,7 +275,6 @@ angular.module('map')
           } else if (layer.format === 'WMS') {
             layer = NxtNonTiledWMSLayer.create(layer);
           }
-          layer.timeState = State.temporal;
         });
       }
 

commit 615a4be91e0478e118f4cf8646a34f2a39691d3e

fix https://github.com/nens/lizard-nxt/issues/880 regression from https://github.com/nens/lizard-client/commit/eef810b75da6a89939ad6fc55cd4724120090624 . non tiled wms layer is supposed to keep track of its own timestate and make independent descisions about whether or not to take actions when syncTime is called

app/components/map/services/map-service.js
@@ -196,9 +196,10 @@ angular.module('map')
         if (lg.isActive() && lg.mapLayers.length > 0) {
           if (lg.temporal) {
             // copy timeState to layers so when added they will respect the
-            // current timestate.
+            // current timestate and can make independent descisions about when
+            // to sync to new timestate.
             angular.forEach(lg.mapLayers, function (layer) {
-              layer.timeState = State.temporal;
+              layer.timeState = angular.copy(State.temporal);
             });
           }
           addLayersRecursively(service._map, lg.mapLayers, 0);