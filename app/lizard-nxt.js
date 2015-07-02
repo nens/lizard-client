a7341e45b630d3c3143ee7aa602e253bbb35ed4a
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2015-07-02 14:16:55 +0200

    set language on  which default to configerd portal locale or nl

--- app/lizard-nxt.js
+++ app/lizard-nxt.js
@@ -90,3 +90,11 @@ angular.module('lizard-nxt')
  */
 angular.module('lizard-nxt')
   .constant('backendDomain', 'https://demo.lizard.net');
+
+/**
+ * @name locale
+ * @memberOf app
+ * @description Portal's default locale.
+ */
+angular.module('lizard-nxt')
+  .constant('defaultLocale', window.locale);
