e99a4253682ac7d9379e954ae398eaeb4e559604
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2015-08-24 11:05:11 +0200

    fixed bug with compile

--- app/components/scenarios/scenarios-directive.js
+++ app/components/scenarios/scenarios-directive.js
@@ -3,13 +3,12 @@
 angular.module('omnibox')
   .directive('scenarios', function () {
     var link =  function (scope) {
-      console.log('i \'m getting another caller: ', scope.$$id)
     };
 
   return {
     link: link,
     templateUrl: 'scenarios/scenarios.html',
-    replace: true,
+    replace: false,
     restrict: 'E'
   };
 
