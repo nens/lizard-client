e1968df97cc57eb6bc861ea5194b4b60b41e8016
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2015-09-28 12:54:13 +0200

    s's

--- app/components/scenarios/scenarios-controller.js
+++ app/components/scenarios/scenarios-controller.js
@@ -24,7 +24,7 @@ angular.module('scenarios')
         page = $scope.page;
       }
       $scope.loading = true;
-      Restangular.one('api/v2/scenario/?page=' + page).get()
+      Restangular.one('api/v2/scenarios/?page=' + page).get()
         .then(function (response) {
           $scope.loading = false;
           // get the amount of pages for the ng-repeater
@@ -43,7 +43,7 @@ angular.module('scenarios')
     $scope.select = function (scenario) {
       if ($scope.selectedScenario === null) {
         $scope.selectedScenario = scenario;
-        Restangular.one('api/v2/scenario/' + scenario.id + '/').get()
+        Restangular.one('api/v2/scenarios/' + scenario.id + '/').get()
           .then(function (response) {
             angular.extend(scenario, response);
           });
