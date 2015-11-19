d5bb2df76e6aae5ecfcb1dd2018d2c92d7ce7e30
Heleen van de Schraaf <heleen.vandeschraaf@nelen-schuurmans.nl>
2015-11-19 14:34:42 +0100

    Refactor annotations to use ngResource.
    Instead of RestAngular or $http.
    RestAngular is no longer maintained so it seems.

--- app/components/annotations/annotations-service.js
+++ app/components/annotations/annotations-service.js
@@ -2,40 +2,40 @@
  * Service to handle annotations retrieval and creation.
  */
 angular.module('annotations')
-  .service("AnnotationsService", ['Restangular', '$http',
-    function (Restangular, $http) {
+  .service("AnnotationsService", ['$resource',
+    function ($resource) {
 
-      var annotationsURL = 'api/v2/annotations';
-
-      var annotationsResource = Restangular.withConfig(
-        function(RestangularConfigurer) {
-          RestangularConfigurer.setRequestSuffix('/');
-          RestangularConfigurer.addResponseInterceptor(function(response, operation) {
-            return response.results;
-          });
-        }).all(annotationsURL);
+      var Annotations = $resource('/api/v2/annotations/:id/', {}, {
+        'query': {
+          method:'GET',
+          isArray:true,
+          transformResponse:
+            function (data, headers) {
+              return angular.fromJson(data).results;
+            }
+        }
+      });
 
       this.getAnnotationsForObject = function (model, id) {
-        return annotationsResource.getList({
+        return Annotations.query({
           object_type__model: model,
           object_id: id
         });
       };
 
-      this.deleteAnnotation = function (annotation) {
-        return annotation.remove();
+      this.deleteAnnotation = function (annotation, success, error) {
+        return Annotations.delete({id: annotation.id}, success, error);
       };
 
-      this.addAnnotationToObject = function (asset, text) {
-        var newAnnotation = {
+      this.addAnnotationToObject = function (asset, text, success, error) {
+        return Annotations.save({
           object_type: asset.entity_name,
           object_id: asset.id,
           text: text,
           datetime_from: new Date().toISOString(),
           datetime_until: new Date().toISOString()
           // picture_url
-        };
-        return $http.post(annotationsURL + '/', newAnnotation);
+        }, success, error);
       };
 
       return this;
