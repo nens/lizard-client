6b9544b8f75f69105d1a8946269383773237d8e3
Arjen Vrielink <arjen.vrielink@gmail.com>
2015-12-09 15:17:40 +0100

    Download CSV rain data from 5minute raster endpoint directly.

--- app/components/omnibox/controllers/rain-controller.js
+++ app/components/omnibox/controllers/rain-controller.js
@@ -14,6 +14,29 @@ angular.module('omnibox')
 
   ) {
 
+    // Hack to get raw rain data when hitting Export
+    // Gets data directly from raster endpoint of raster RAW_RAIN_RASTER_UUID
+    // limited to MAX_TIME_INTERVAL in ms
+    $scope.$watch(State.toString('temporal.timelineMoving'), function (n, o) {
+      var RAW_RAIN_RASTER_UUID = '827dfde3-e3eb-46ee-b671-ca36e922dbb6',
+          MAX_TIME_INTERVAL = 86400000 * 365.2425 / 12;  // 1 month
+
+      if (State.temporal.end - State.temporal.start < MAX_TIME_INTERVAL) {
+        $scope.rawDataUrl =
+          'http://localhost:8000/api/v2/rasters/' +
+          RAW_RAIN_RASTER_UUID + '/' +
+          'data/?format=csv' +
+          '&start=' + 
+          new Date(State.temporal.start).toISOString().split('.')[0] +
+          '&stop=' +
+          new Date(State.temporal.end).toISOString().split('.')[0] +
+          '&geom=' + UtilService.geomToWkt(State.spatial.here) +
+          '&srs=EPSG:4326';
+      } else {
+        $scope.rawDataUrl = '.';
+      }
+    });
+
       /*
        * @description
        * angular isolate scope is messed with
@@ -23,7 +46,6 @@ angular.module('omnibox')
       $scope.$watch('box.fullDetails.rain', function (n) {
         $scope.fullDetails = n;
       });
-
 
       $scope.rrc = {
         active: false
@@ -41,7 +63,6 @@ angular.module('omnibox')
           $scope.lg.layers['rain'].changed = !$scope.lg.layers['rain'].changed;
         }
       };
-
 
       $scope.$watch("lg.layers['rain'].changed", function (n, o) {
         if (n === o || !$scope.rrc.active) { return; }
