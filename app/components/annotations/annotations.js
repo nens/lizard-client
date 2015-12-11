338c6848673a746c34fe472b230a8e8b3b9bcb24
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2015-12-11 16:58:28 +0100

    Releasing with Dist: 2.4.2

--- app/components/annotations/annotations.js
+++
@@ -1,6 +0,0 @@
-/**
- * @module
- * @description Annotations module shows an assets annotations and adds support
- *              for adding annotations to an asset.
- */
-angular.module('annotations', ['ngResource']);
