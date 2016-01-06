3cf1d2db7bf6c825f542a6dc3400e8ff15f3cf5b
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2016-01-06 16:25:39 +0100

    wip2

--- app/components/omnibox/controllers/dashboard-cards-controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('omnibox')
-  .controller("DashboardCtrl", ["State", function (State) {
-
-  this.selected = State.selected;
-
-}]);
--- app/components/omnibox/directives/db-asset-card-directive.js
+++ app/components/omnibox/directives/db-asset-card-directive.js
@@ -1,17 +1,38 @@
 
 angular.module('omnibox')
-  .directive('dbAssetCard', ['WantedAttributes',
-    function (WantedAttributes) {
+  .directive('dbAssetCard', [ '$http', 'WantedAttributes', 'TimeseriesService',
+    function ($http, WantedAttributes, TimeseriesService) {
   return {
     link: function (scope) {
       scope.wanted = WantedAttributes;
+      scope.asset = {};
 
-      console.log(scope.id);
+      scope.$watch('assetId', function () {
+        var entity = scope.assetId.split('$')[0];
+        var id = scope.assetId.split('$')[1];
+
+        $http({
+          url: 'api/v2/' + entity + 's' + '/' + id + '/',
+          method: 'GET'
+        })
+
+        .then(function (response) {
+          response.data.entity_name = entity;
+          scope.asset = response.data;
+        });
+
+        TimeseriesService.getTimeSeriesForObject(scope.assetId)
+
+        .then(function(response) {
+          scope.ts = response.results;
+        });
+
+     });
 
     },
     restrict: 'E',
     scope: {
-      id: '='
+      assetId: '='
     },
     replace: true,
     templateUrl: 'omnibox/templates/db-asset-card.html'
