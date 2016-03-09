919c7a3355aef560ec73d6f8b18cf42a4e5be41a
Heleen van de Schraaf <heleen.vandeschraaf@nelen-schuurmans.nl>
2016-03-09 16:38:09 +0100

    Make Lizard Apps screen loading dynamic.

--- app/components/user-menu/user-menu-directive.js
+++ app/components/user-menu/user-menu-directive.js
@@ -3,10 +3,30 @@
  * Shows user-menu and has logout login buttons
  */
 angular.module('user-menu')
-  .directive('userMenu', function () {
+  .directive('userMenu', ['UtilService', '$location',
+              function (UtilService, $location) {
 
     var link = function (scope, element, attrs) {
-      if (window.Lizard) { window.Lizard.startPlugins() }; // jshint ignore:line
+
+      var appsScreenUrl = function() {
+        var appsScreenSlug = UtilService.slugify($location.host());
+        return "//apps.lizard.net/screens/" + appsScreenSlug + ".js";
+      };
+
+      var script = document.createElement('script');
+      script.src = appsScreenUrl();
+      script.onload = function () {
+        if (typeof window.Lizard.startPlugins === 'function') {
+          window.Lizard.startPlugins(); // jshint ignore:line
+          scope.$digest();
+        }
+      };
+
+      document.head.appendChild(script);
+
+      scope.showApps = function() {
+        return $('#lizard-apps-button').children().length > 0;
+      };
     };
 
     return {
@@ -14,4 +34,4 @@ angular.module('user-menu')
       link: link,
       templateUrl: 'user-menu/user-menu.html'
     };
-  });
+  }]);
