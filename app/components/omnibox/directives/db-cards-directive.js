8c4552d4e4b25fcd3f14da0cdb6f6fd74373ca17
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2016-03-11 13:26:37 +0100

    fix drag and drop with different valuetypes

--- app/components/omnibox/directives/db-cards-directive.js
+++ app/components/omnibox/directives/db-cards-directive.js
@@ -10,13 +10,7 @@ angular.module('omnibox')
       var emulateClick = function (el) {
         // other plottable item. Toggle on drag to put element in their own
         // plot.
-        DragService.destroy();
-        console.log('cllick the moferf4fck');
-        el.dispatchEvent(new MouseEvent("click", {
-          "view": window,
-          "bubbles": true,
-          "cancelable": false
-        }));
+        element.find('#' + el.dataset.uuid).click();
       };
 
       DragService.on('drop', function (el, target, source) {
@@ -28,47 +22,40 @@ angular.module('omnibox')
         // NOTE: there is only one drop callbakc for all the possible assets. So
         // instead of searching for the ts in scope.asset.timeseries, all the
         // assets are searched.
-        if (uuid) {
-          // timeseries
-          var ts, otherGraphTS, otherCompatibleGraph;
+        // timeseries
+        var ts, otherGraphTS, otherCompatibleGraph;
 
-          _.forEach(DataService.assets, function (asset) {
-            ts = _.find(asset.timeseries, function (ts) {
-              return ts.uuid === uuid;
-            });
-            return ts === undefined; // if true: continue
+        _.forEach(DataService.assets, function (asset) {
+          ts = _.find(asset.timeseries, function (ts) {
+            return ts.uuid === uuid;
+          });
+          return ts === undefined; // if true: continue
+        });
+
+
+        _.forEach(DataService.assets, function (asset) {
+          otherGraphTS = _.find(asset.timeseries, function (ts) {
+            return ts.order === order && ts.active;
           });
 
+          return otherGraphTS === undefined; // if true: continue
+        });
 
-          _.forEach(DataService.assets, function (asset) {
-            otherGraphTS = _.find(asset.timeseries, function (ts) {
-              return ts.order === order && ts.active;
-            });
-
-            otherCompatibleGraph = _.find(asset.timeseries, function (ts) {
-              return ts.value_type === ts.value_type;
-            });
-
-            return otherGraphTS === undefined; // if true: continue
-          });
-
-          if (ts.value_type !== otherGraphTS.value_type && otherGraphTS !== undefined) {
-            notie.alert(2,
-              gettextCatalog.getString('Whoops, the graphs are not the same type. Try again, or click!'));
-            // emulateClick(el);
-          } else if (otherGraphTS === undefined) {
-            ts.order = order || 0; // dashboard could be empty
-            ts.active = true;
-            State.selected.timeseries = _.union(
-              State.selected.timeseries,
-              [ts.uuid]
-            );
-          }
-        }
-        else {
-          // i'm fairly certain this never gets called, and nothing is ever done
-          // when it does actually happen.
+        if (otherGraphTS === undefined) {
           emulateClick(el);
+        }
+
+        else if (ts.value_type !== otherGraphTS.value_type) {
+          notie.alert(2,
+            gettextCatalog.getString('Whoops, the graphs are not the same type. Try again!'));
+          emulateClick(el);
+        } else {
+          ts.order = order || 0; // dashboard could be empty
+          ts.active = true;
+          State.selected.timeseries = _.union(
+            State.selected.timeseries,
+            [ts.uuid]
+          );
         }
 
         el.remove();
