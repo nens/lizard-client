49b0324b53678bd38ba78119666ca0acabe15108
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2016-03-11 10:56:10 +0100

    Fix image timeserie not being plotted in line chart
    * Added icons for images and timeseries
    * Image timeseries cannot be added to lines and vice versa

--- app/components/omnibox/directives/db-cards-directive.js
+++ app/components/omnibox/directives/db-cards-directive.js
@@ -1,7 +1,7 @@
 
 angular.module('omnibox')
-  .directive('dbCards', [ 'State', 'DataService', 'DragService',
-    function (State, DataService, DragService) {
+  .directive('dbCards', [ 'State', 'DataService', 'DragService', 'gettextCatalog', 'notie',
+    function (State, DataService, DragService, gettextCatalog, notie) {
   return {
     link: function (scope, element) {
 
@@ -10,6 +10,8 @@ angular.module('omnibox')
       var emulateClick = function (el) {
         // other plottable item. Toggle on drag to put element in their own
         // plot.
+        DragService.destroy();
+        console.log('cllick the moferf4fck');
         el.dispatchEvent(new MouseEvent("click", {
           "view": window,
           "bubbles": true,
@@ -28,7 +30,7 @@ angular.module('omnibox')
         // assets are searched.
         if (uuid) {
           // timeseries
-          var ts, otherGraphTS;
+          var ts, otherGraphTS, otherCompatibleGraph;
 
           _.forEach(DataService.assets, function (asset) {
             ts = _.find(asset.timeseries, function (ts) {
@@ -42,21 +44,30 @@ angular.module('omnibox')
             otherGraphTS = _.find(asset.timeseries, function (ts) {
               return ts.order === order && ts.active;
             });
+
+            otherCompatibleGraph = _.find(asset.timeseries, function (ts) {
+              return ts.value_type === ts.value_type;
+            });
+
             return otherGraphTS === undefined; // if true: continue
           });
 
-          if (otherGraphTS === undefined || ts.value_type !== otherGraphTS.value_type) {
-            emulateClick(el);
+          if (ts.value_type !== otherGraphTS.value_type && otherGraphTS !== undefined) {
+            notie.alert(2,
+              gettextCatalog.getString('Whoops, the graphs are not the same type. Try again, or click!'));
+            // emulateClick(el);
+          } else if (otherGraphTS === undefined) {
+            ts.order = order || 0; // dashboard could be empty
+            ts.active = true;
+            State.selected.timeseries = _.union(
+              State.selected.timeseries,
+              [ts.uuid]
+            );
           }
-
-          ts.order = order || 0; // dashboard could be empty
-          ts.active = true;
-          State.selected.timeseries = _.union(
-            State.selected.timeseries,
-            [ts.uuid]
-          );
         }
         else {
+          // i'm fairly certain this never gets called, and nothing is ever done
+          // when it does actually happen.
           emulateClick(el);
         }
 
