ed793ad26b159f8ee5938b6136189fca95cc8611
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2016-04-04 16:32:05 +0200

    Fix bower dep injection, add comment and newlines

--- app/components/data-menu/services/data-service.js
+++ app/components/data-menu/services/data-service.js
@@ -578,10 +578,14 @@ angular.module('data-menu')
         }
 
         angular.forEach(this.layerGroups, function (layerGroup) {
-          // UTF has a special status and is not queried in this loop.
           if (
+
+            // UTF has a special status and is not queried in this loop.
             (instance.utfLayerGroup && (layerGroup.slug === instance.utfLayerGroup.slug))
+
+            // One too many dimension
             || (layerGroup.temporal && geo.geometry.type === 'LineString')
+
           ) {
             return;
           }
