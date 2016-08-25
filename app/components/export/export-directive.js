323061dd53620b725172d5fd2df4766f5adbaea1
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2016-08-25 15:18:17 +0200

    keep track of selected timeseries and get uuids when export

--- app/components/export/export-directive.js
+++ app/components/export/export-directive.js
@@ -3,11 +3,20 @@ angular.module('export')
 .directive('exportSelector', ['DataService', function (DataService) {
   var link = function (scope) {
 
+
     scope.assets = DataService.assets;
 
-  scope.export = function (assets) {
-    console.log(assets);
-  };
+    scope.toExport = {};
+
+    scope.export = function () {
+      var uuids =_.map(scope.toExport, function (yes, uuid) {
+        if (yes) { return uuid; }
+      }).join(',');
+
+      // Request timeseries/data/ with uuids and format=csvzip and async=true
+
+      console.log(scope.toExport, uuids);
+    };
 
   };
 
