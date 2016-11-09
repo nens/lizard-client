b1cc1d154d83d040ea010acc69250a6c6493ed30
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2016-11-09 09:04:16 +0100

    chore: little refactoring

--- app/components/legend/legend-directive.js
+++ app/components/legend/legend-directive.js
@@ -1,18 +1,19 @@
 angular.module('lizard-nxt')
 .directive('legend', ["LegendService", "State", function(LegendService, State) {
 
-  var link = function (scope, element, attrs) {
+  var _updateData = function (bounds, layers) {
+    LegendService.updateLegendData(bounds, layers);
+    console.log("LegendService.rasterData:", LegendService.rasterData);
+  };
 
+  var link = function (scope, element, attrs) {
     scope.$watch(State.toString('layers.active'), function (n, o) {
       if (n === o) { return; }
-      LegendService.updateLegendData(scope.state.spatial.bounds, scope.state.layers);
-      console.log("[dbg] LegendService.rasterData (i):", LegendService.rasterData);
+      _updateData(scope.state.spatial.bounds, scope.state.layers);
     });
-
     scope.$watch('state.spatial.bounds', function (n, o) {
       if (n === o) { return; }
-      LegendService.updateLegendData(n, scope.state.layers);
-      console.log("[dbg] LegendService.rasterData (ii):", LegendService.rasterData);
+      _updateData(n, scope.state.layers);
     });
   };
 
