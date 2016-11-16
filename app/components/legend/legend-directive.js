4d2966000585f58969ad599b39e4a89e9b4b7fee
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2016-11-16 10:57:06 +0100

    seperate cards for discrete vs. continuous

--- app/components/legend/legend-directive.js
+++ app/components/legend/legend-directive.js
@@ -1,14 +1,13 @@
 angular.module('legend')
 .directive('legend', ["LegendService", "State", function(LegendService, State) {
 
-  // When using this directive to actually draw the legend in the browser, you
-  // only need to read from scope.legendData.
-
   var link = function (scope, element, attrs) {
+
+    /* scope variables used for DISCRETE rasters: ****************************/
 
     scope.MAX_DISCRETE_CATEGORIES_DEFAULT = 5;
     scope.showAllCategoriesForRaster = {};
-    scope.selectedRasterName = null;
+    scope.selectedDiscreteRasterName = null;
 
     scope.getBorderStyle = function (datum) {
       return datum.label === -1 ? "1px solid #ccc" : "0";
@@ -16,15 +15,6 @@ angular.module('legend')
 
     scope.totalCategoryCount = function (rasterName) {
       return scope.legendData.discrete[rasterName].length;
-    };
-
-    scope.shownCategoryCount = function (rasterName) {
-      // return scope.legendData.discrete[rasterName].length;
-
-      return Math.min(
-        scope.MAX_DISCRETE_CATEGORIES_DEFAULT,
-        scope.totalCategoryCount(rasterName)
-      );
     };
 
     scope.showingAllCategories = function (rasterName) {
@@ -45,7 +35,8 @@ angular.module('legend')
     };
 
     scope.hasMoreCategoriesAvailableThanDefault = function (rasterName) {
-      return scope.totalCategoryCount(rasterName) > scope.MAX_DISCRETE_CATEGORIES_DEFAULT;
+      return scope.totalCategoryCount(rasterName) >
+        scope.MAX_DISCRETE_CATEGORIES_DEFAULT;
     };
 
     scope.discreteRasterIsSelected = function (rasterName) {
@@ -53,34 +44,77 @@ angular.module('legend')
       if (allRasterNames.length === 1 && allRasterNames[0] === rasterName) {
         // If only a single discrete raster is present, that must be the
         // selected discrete raster:
-        scope.selectedRasterName = rasterName;
+        scope.selectedDiscreteRasterName = rasterName;
         return true;
       }
-      else if (!scope.selectedRasterName) {
+      else if (!scope.selectedDiscreteRasterName) {
         // If currently no discrete raster is the selected one, we choose the
         // first we encounter (used on initial load):
-        scope.selectedRasterName = rasterName;
+        scope.selectedDiscreteRasterName = rasterName;
         return true;
       } else {
-        return scope.selectedRasterName === rasterName;
+        return scope.selectedDiscreteRasterName === rasterName;
       }
     };
 
-    scope.switchSelectedRaster = function (currentRasterName) {
+    scope.switchSelectedDiscreteRaster = function (currentRasterName) {
       var allRasterNames = Object.keys(scope.legendData.discrete);
       var currentIndex = allRasterNames.indexOf(currentRasterName);
       var nextIndex = (currentIndex + 1) % allRasterNames.length;
-      scope.selectedRasterName = allRasterNames[nextIndex];
+      scope.selectedDiscreteRasterName = allRasterNames[nextIndex];
     };
+
+    /* scope variables used for CONTINUOUS rasters: **************************/
+
+    scope.selectedContinuousRasterName = null;
+
+    scope.continuousRasterIsSelected = function (rasterName) {
+      var allRasterNames = Object.keys(scope.legendData.continuous);
+      if (allRasterNames.length === 1 && allRasterNames[0] === rasterName) {
+        // If only a single continuous raster is present, that must be the
+        // selected continuous raster:
+        scope.selectedContinuousRasterName = rasterName;
+        return true;
+      }
+      else if (!scope.selectedDiscreteRasterName) {
+        // If currently no discrete raster is the selected one, we choose the
+        // first we encounter (used on initial load):
+        scope.selectedDiscreteRasterName = rasterName;
+        return true;
+      } else {
+        return scope.selectedDiscreteRasterName === rasterName;
+      }
+    };
+
+    scope.round = function (r, decimalCount) {
+      var d = decimalCount === undefined ? 0 : decimalCount;
+      var multiplier = Math.pow(10, d);
+      return Math.round(r * multiplier) / multiplier;
+    };
+
+    scope.buildGradient = function (rasterName) {
+      console.log("[F] buildGradient");
+      var min = scope.legendData[rasterName].min;
+      var max = scope.legendData[rasterName].max;
+      var allColors = scope.legendData[rasterName].colormap.interp;
+      var selectedColors = [];
+      angular.forEach(allColors, function (v, k) {
+
+
+      });
+    };
+
+    /* 2x $watch used for triggering changes in both the discrete and
+       continuous legends: */
 
     scope.$watch(State.toString('layers.active'), function (n, o) {
       if (n === o) { return; }
-
       scope.legendData = LegendService.updateLegendData(
         State.spatial.bounds,
         scope.state.layers);
       console.log("legendData:", scope.legendData);
     });
+
     scope.$watch('state.spatial.bounds', function (n, o) {
       if (n === o) { return; }
       scope.legendData = LegendService.updateLegendData(n, scope.state.layers);
