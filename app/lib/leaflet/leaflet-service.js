cedfe0e468e960ee2addefd933e2533d2f151046
ernstkui <ernst.kuilder@nelen-schuurmans.nl>
2016-11-28 17:52:56 +0100

    feat(regions): initial commit of nxtAjaxGeoJSON layer.
    - An extended geojson layer which accepts a request url instead of geojson.

--- app/lib/leaflet/leaflet-service.js
+++ app/lib/leaflet/leaflet-service.js
@@ -8,7 +8,10 @@
  * Perhaps in the future this can be done with CommonJS style requires.
  */
 angular.module('lizard-nxt')
-  .service('LeafletService', [function () {
+.service('LeafletService', ['$http', function ($http) {
+
+  var L = window.L;
+
   if (L) {
     // Leaflet global variable to speed up vector layer,
     // see: http://leafletjs.com/reference.html#path-canvas
@@ -19,8 +22,120 @@ angular.module('lizard-nxt')
     // within 0.0001 degree of the click.
     L.LatLng.MAX_MARGIN = 0.0001;
 
-    return L;
-  } else {
+
+    /**
+     * Nxt ajaxified leaflet geojson layer.
+     *
+     * An extended L.GeoJSON. Instead of accepting geojson it accepts a url,
+     * request parameters and booleans to update on bbox/zoom. The request
+     * should return geojson in response.results.
+     *
+     * if bbox in options is truthy, it takes the bbox from the map and adds it
+     * as the in_bbox parameter. Same for zoom, as z parameter.
+     *
+     */
+    L.NxtAjaxGeoJSON = L.GeoJSON.extend({
+
+      options: {
+        requestUrl: null, // GeoJSON request URL.
+        requestParams: {}, // GeoJSON request args.
+      },
+
+      initialize: function(requestUrl, options) {
+        // L.GeoJSON init with blank content as we will get it later.
+        this.options.requestUrl = requestUrl;
+        L.GeoJSON.prototype.initialize.call(this, null, options);
+      },
+
+      onAdd: function(map) {
+        L.GeoJSON.prototype.onAdd.call(this, map);
+
+        this.reload(); // Load it at the beginning.
+
+        if (this.options.bbox) { // Reload if the map pans
+          this._map.on('moveend', this.reload, this);
+        }
+
+        if (this.options.zoom) { // Reload if the map zooms
+          this._map.on('zoomend', this.reload, this);
+        }
+      },
+
+      onRemove: function(map) {
+        this._map.off('moveend', this.reload, this);
+        this._map.off('zoomend', this.reload, this);
+
+        L.GeoJSON.prototype.onRemove.call(this, map);
+      },
+
+      // Build the final request parameters to send to the server
+      _getParams: function() {
+        var requestParams = typeof this.options.requestParams === 'function'
+          ? this.options.requestParams.call(this, this)
+          : this.options.requestParams;
+
+        // Add bbox param if necessary
+        if (this.options.bbox && this._map) {
+          requestParams.in_bbox = this._map.getBounds().toBBoxString();
+        }
+        if (this.options.zoom && this._map) {
+          requestParams.z = this._map.getZoom();
+        }
+
+        return requestParams;
+      },
+
+      reload: function() {
+
+        // Use $http to respect loading bar.
+        $http.get(this.options.requestUrl, {params: this._getParams()})
+        .then(this._checkResponseStatus)
+        .then(this.redraw.bind(this));
+
+      },
+
+      _checkResponseStatus: function(response) {
+
+        if (response.status === 200) {
+          return response.data.results;
+        }
+        else if (response.status) {
+          console.log(
+            'ajaxRequest error status = '
+            + response.status
+            + ' calling '
+            + this._getUrl()
+          );
+          return [];
+        }
+      },
+
+      redraw: function(json) {
+        // Empty the layer
+        for (var l in this._layers) {
+          if (this._map) {
+            this._map.removeLayer(this._layers[l]);
+          }
+        }
+
+        this._layers = [];
+
+        // Add the data to the layer
+        this.addData(json);
+
+      }
+
+    });
+
+    L.nxtAjaxGeoJSON = function(url, options) {
+      return new L.NxtAjaxGeoJSON(url, options);
+    };
+  }
+
+
+  else {
     throw new Error('Leaflet can not be found');
   }
+
+  return L;
 }]);
