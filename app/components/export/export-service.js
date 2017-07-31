1f5eef59b98ab2f13d727bbfabdb7c2e95d93fa4
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2017-07-31 11:26:04 +0200

    rm console.log

--- app/components/export/export-service.js
+++ app/components/export/export-service.js
@@ -3,7 +3,6 @@ angular.module('export').service('ExportService', function () {
   return {
     isPolling: { value: false },
     setIsPolling: function (newValue) {
-      console.log("[F] setIsPolling; newValue =", newValue);
       this.isPolling.value = newValue;
     }
   }
