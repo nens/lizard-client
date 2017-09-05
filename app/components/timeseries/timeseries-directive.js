c5e531393f7602e4a4b29410a78abb2c59dc23a7
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2017-09-05 11:09:17 +0200

    TS interval selectable according to Gijs' design

--- app/components/timeseries/timeseries-directive.js
+++ app/components/timeseries/timeseries-directive.js
@@ -35,17 +35,9 @@ angular.module('timeseries')
         SelectionService.initializeRaster(selectionObject, selectionType);
       });
 
-      scope.zoomToMagic = function (value_type) {
+      scope.zoomToInterval = function (value_type, intervalText) {
 
-        // For the DDSC-only "timeseries-with-images" we currently not support
-        // zooming to the data. Since this data is not available locally/in
-        // dev, because Ijkeldijk never payed for this new functionality, I
-        // cannot *guarantee* it's correct working. In theory, though, it should
-        // already work as-is.
-        //
-        // However, when we can have our (read: Roel's) shared data set
-        // updated to contain at least one timeseries with images I'll be more
-        // than happy to build it.
+        // return early - if the timeseries consists of images
         if (value_type === 'image') {
           notie.alert(3,
             gettextCatalog.getString(
@@ -55,42 +47,74 @@ angular.module('timeseries')
           return;
         }
 
-        if (State.context === 'map' &&
-            State.selections &&
-            State.selections.length >= 1) {
-
-          var activeTsUUID = _.find(State.selections,
-            { active: true }).timeseries;
-
-          var activeTs = _.find(TimeseriesService.timeseries,
-            { id: activeTsUUID });
-
-          // We toggle State.temporal.timelineMoving to trigger the correct
-          // angular $watch in the timeline-directive.
-          State.temporal.timelineMoving = !State.temporal.timelineMoving;
-
-          var start = activeTs.start;
-          var end = activeTs.end;
-
-          // If start and end are at the same point in time (i.e. we have a
-          // timeseries with only a single measured value), we "pad" the space
-          // to have decent visualization.
-          if (start === end) {
-            var defaultAggWindow = 3600000; // 1 hour, in ms
-            var aggWindow = State.temporal.aggWindow || defaultAggWindow;
-            end = start + aggWindow * 10;
-          }
-
-          State.temporal.at = start;
-          State.temporal.start = start;
-          State.temporal.end = end;
-
-          $timeout(function () {
-            // We reset State.temporal.timelineMoving (a-sync, compliant with
-            // angular $digest cycle).
-            State.temporal.timelineMoving = !State.temporal.timelineMoving;
-          });
+        // return early - if we're not in map ctx
+        // return early - if we don't have a timeseries selected
+        if (!(State.context === 'map' &&
+              State.selections &&
+              State.selections.length >= 1)) {
+          return;
         }
+
+        var now = (new Date()).getTime();
+        var start, end, intervalMs;
+        switch(intervalText) {
+          case "one_year":
+            intervalMs = 31536000000; // one year in ms
+            end = now;
+            start = end - intervalMs;
+            break;
+          case "three_months":
+            intervalMs = 7884000000; // three months in ms (avg)
+            end = now;
+            start = end - intervalMs;
+            break;
+          case "two_weeks":
+            intervalMs = 1209600000; // two weeks in ms
+            end = now;
+            start = end - intervalMs;
+            break;
+          case "timesteps_range":
+
+            var activeTsUUID = _.find(State.selections,
+              { active: true }).timeseries;
+
+            var activeTs = _.find(TimeseriesService.timeseries,
+              { id: activeTsUUID });
+
+            start = activeTs.start;
+            end = activeTs.end;
+
+            // If start and end are at the same point in time (i.e. we have a
+            // timeseries with only a single measured value), we "pad" the space
+            // to have decent visualization.
+            if (start === end) {
+              var defaultAggWindow = 3600000; // 1 hour, in ms
+              var aggWindow = State.temporal.aggWindow || defaultAggWindow;
+              end = start + aggWindow * 10;
+            }
+
+            break;
+          default:
+            console.error(
+              "Unknown interval '" +
+              intervalText +
+              "' for temporal zoom; allowed values are " +
+              "'one_year', 'three_months', 'two_weeks'"
+            );
+        }
+
+        // We toggle State.temporal.timelineMoving to trigger the correct
+        // angular $watch in the timeline-directive.
+        State.temporal.timelineMoving = !State.temporal.timelineMoving;
+        State.temporal.at = start;
+        State.temporal.start = start;
+        State.temporal.end = end;
+
+        $timeout(function () {
+          // We reset State.temporal.timelineMoving (a-sync, compliant with
+          // angular $digest cycle).
+          State.temporal.timelineMoving = !State.temporal.timelineMoving;
+        });
       };
 
       scope.$watch(selectionType, function () {
@@ -102,7 +126,6 @@ angular.module('timeseries')
       });
 
       scope.$on('$destroy', function () {
-
         if (State.assets.length > 1 && State.context === 'map') {
           _.forEach(State.selections, function (selection) {
             selection.active = false;
