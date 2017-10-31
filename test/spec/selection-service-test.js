b1d49d96a1fa0b94c2be4d16c7e05af65cd3dbad
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2017-10-31 14:38:55 +0100

    Reinstated all-but-one tests for selections

--- test/spec/selection-service-test.js
+++ test/spec/selection-service-test.js
@@ -1,86 +1,86 @@
-// 'use strict';
+'use strict';
 
-// describe('selection service', function () {
+describe('selection service', function () {
 
-//   var SelectionService;
+  var SelectionService;
 
-//   beforeEach(function () {
-//     module('lizard-nxt');
-//     module('global-state');
-//   });
+  beforeEach(function () {
+    module('lizard-nxt');
+    module('global-state');
+  });
 
-//   beforeEach(inject(function ($injector) {
-//     SelectionService = $injector.get('SelectionService');
-//   }));
+  beforeEach(inject(function ($injector) {
+    SelectionService = $injector.get('SelectionService');
+  }));
 
-//   it('should indicate support for the correct properties ', function () {
+  it('should indicate support for the correct properties ', function () {
 
-//     expect(SelectionService.dbSupportedData).toBeDefined();
-//     expect(SelectionService.dbSupportedData(
-//         'Point', { temporal: {} })).toBe(true);
-//     expect(SelectionService.dbSupportedData(
-//         'Polygon', { scale: 'ratio' })).toBe(true);
-//     expect(SelectionService.dbSupportedData(
-//         'LineString', { format: 'Vector' })).toBe(true);
-//     expect(SelectionService.dbSupportedData('Point', 'ordinal')).toBe(false);
-//     expect(SelectionService.dbSupportedData(
-//         '', { scale: 'nominal' })).toBe(false);
+    expect(SelectionService.dbSupportedData).toBeDefined();
+    expect(SelectionService.dbSupportedData(
+        'Point', { temporal: {} })).toBe(true);
+    expect(SelectionService.dbSupportedData(
+        'Polygon', { scale: 'ratio' })).toBe(true);
+    expect(SelectionService.dbSupportedData(
+        'LineString', { format: 'Vector' })).toBe(true);
+    expect(SelectionService.dbSupportedData('Point', 'ordinal')).toBe(false);
+    expect(SelectionService.dbSupportedData(
+        '', { scale: 'nominal' })).toBe(false);
 
-//   });
+  });
 
-//   it('should return a false match for a non existent raster', function () {
+  it('should return a false match for a non existent raster', function () {
 
-//     var rasterSelection = {
-//       geom: "10000.0000,520000.0000",
-//       raster: "doesn1t0exist",
-//       type: "raster"
-//     };
+    var rasterSelection = {
+      geom: "10000.0000,520000.0000",
+      raster: "doesn1t0exist",
+      type: "raster"
+    };
 
-//     expect(SelectionService.rasterMetaData).toBeDefined();
-//     expect(SelectionService.rasterMetaData(undefined)).toBeDefined();
-//     expect(SelectionService.rasterMetaData(undefined, rasterSelection))
-//         .toEqual({ match: false });
-//   });
+    expect(SelectionService.rasterMetaData).toBeDefined();
+    expect(SelectionService.rasterMetaData(undefined)).toBeDefined();
+    expect(SelectionService.rasterMetaData(undefined, rasterSelection))
+        .toEqual({ match: false });
+  });
 
-//   it('should return a false match for a non existent timeseries', function () {
+  it('should return a false match for a non existent timeseries', function () {
 
-//     var timeseriesSelection = {
-//       timeseries: "66c94920-706e-42e4-8b8c-e0c9e0e2d5db",
-//       type: "timeseries"
-//     };
+    var timeseriesSelection = {
+      timeseries: "66c94920-706e-42e4-8b8c-e0c9e0e2d5db",
+      type: "timeseries"
+    };
 
-//     expect(SelectionService.timeseriesMetaData).toBeDefined();
-//     expect(SelectionService.timeseriesMetaData(undefined))
-//         .toBeDefined();
-//     expect(SelectionService.timeseriesMetaData(
-//         undefined, timeseriesSelection)).toEqual({ match: false });
-//   });
+    expect(SelectionService.timeseriesMetaData).toBeDefined();
+    expect(SelectionService.timeseriesMetaData(undefined))
+        .toBeDefined();
+    expect(SelectionService.timeseriesMetaData(
+        undefined, timeseriesSelection)).toEqual({ match: false });
+  });
 
 
-//   it('should toggle active state in selections', function () {
+  // it('should toggle active state in selections', function () {
 
-//     var selectionActive = {
-//       uuid: "1234",
-//       active: true,
-//       geom: "10000.0000,520000.0000",
-//       order: 1,
-//       type: "raster"
-//     };
-//     var selectionInActive = {
-//       uuid: "5678",
-//       active: false,
-//       order: 0,
-//       timeseries: "66c94920-706e-42e4-8b8c-e0c9e0e2d5db",
-//       type: "timeseries"
-//     };
+  //   var selectionActive = {
+  //     uuid: "1234",
+  //     active: true,
+  //     geom: "10000.0000,520000.0000",
+  //     order: 1,
+  //     type: "raster"
+  //   };
+  //   var selectionInActive = {
+  //     uuid: "5678",
+  //     active: false,
+  //     order: 0,
+  //     timeseries: "66c94920-706e-42e4-8b8c-e0c9e0e2d5db",
+  //     type: "timeseries"
+  //   };
 
-//     SelectionService.toggle(selectionActive);
-//     SelectionService.toggle(selectionInActive);
+  //   SelectionService.toggle(selectionActive);
+  //   SelectionService.toggle(selectionInActive);
 
-//     expect(SelectionService.toggle).toBeDefined();
-//     expect(selectionActive.active).toBe(false);
-//     expect(selectionActive.order).toBe(1);
-//     expect(selectionInActive.active).toBe(true);
-//     expect(selectionInActive.order).toBe(0);
-//   });
-// });
+  //   expect(SelectionService.toggle).toBeDefined();
+  //   expect(selectionActive.active).toBe(false);
+  //   expect(selectionActive.order).toBe(1);
+  //   expect(selectionInActive.active).toBe(true);
+  //   expect(selectionInActive.order).toBe(0);
+  // });
+});
