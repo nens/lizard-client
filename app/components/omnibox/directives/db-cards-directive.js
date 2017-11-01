0f17d30ffa14f1c4acdb31a79a7e317528cc1eab
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2017-11-01 14:36:37 +0100

    DB geom cards only who when required

--- app/components/omnibox/directives/db-cards-directive.js
+++ app/components/omnibox/directives/db-cards-directive.js
@@ -11,6 +11,7 @@ angular.module('omnibox')
     'DBCardsService',
     'ChartCompositionService',
     '$timeout',
+    'DataService',
     function (
       State,
       SelectionService,
@@ -21,7 +22,8 @@ angular.module('omnibox')
       TimeseriesService,
       DBCardsService,
       ChartCompositionService,
-      $timeout) {
+      $timeout,
+      DataService) {
   return {
     link: function (scope, element) {
 
@@ -202,6 +204,22 @@ angular.module('omnibox')
         // Remove drag element.
         el.parentNode.removeChild(el);
       });
+
+      scope.mustShowGeomCard = function (geom) {
+        var activeRasters = _.filter(State.layers, function (layer) {
+          return layer.active && layer.type === "raster";
+        });
+        var activeTemporalRasterCount = 0;
+        DataService.dataLayers.forEach(function (dataLayer) {
+          if (dataLayer.temporal) {
+            var activeTemporalraster = _.find(activeRasters, { uuid: dataLayer.uuid });
+            if (activeTemporalraster) {
+              activeTemporalRasterCount += 1;
+            }
+          }
+        });
+        return activeTemporalRasterCount !== 0;
+      };
 
       scope.$on('$destroy', function () {
         DragService.destroy();
