1e9e71f38307eee0ccf86de0b9f8150405ad3475
Remco Gerlich <remco.gerlich@nelen-schuurmans.nl>
2017-11-30 16:42:50 +0100

    Remove getActiveCountAndOrder

--- app/components/omnibox/services/db-cards-service.js
+++ app/components/omnibox/services/db-cards-service.js
@@ -21,49 +21,10 @@ angular.module('omnibox')
     }
   }
 
-  /**
-   * Loops over all the items that can be plotted and return the count and the
-   * highest order.
-   *
-   * @return {{count: int, order: int}}
-   */
-  var getActiveCountAndOrder = function () {
-
-    var orders = [];
-    var actives = 0;
-
-    _.forEach(
-      State.selections,
-      function (selection) {
-        if (selection.active) {
-          actives++;
-          orders.push(selection.order);
-        }
-      }
-    );
-
-    DataService.assets.forEach(function (asset) {
-
-      if (asset.entity_name === 'leveecrosssection' &&
-        asset.crosssection.active) {
-        actives++;
-        orders.push(asset.crosssection.order);
-      }
-    });
-
-    return {
-      count: actives,
-      order: _.max(orders)
-    };
-  };
-
   return {
-    getActiveCountAndOrder: getActiveCountAndOrder,
-
     colorPickersSettings: colorPickersSettings,
     openColorPicker: openColorPicker,
     closeColorPicker: closeColorPicker,
     closeAllColorPickers: closeAllColorPickers
   };
-
 }]);
