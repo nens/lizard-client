772b5dfb33c35de144409bec3d3574d56b3784bb
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2017-12-11 15:53:15 +0100

    git rm'd old files

--- app/components/export/export-service.js
+++
@@ -1,8 +0,0 @@
-angular.module('export').service('ExportService', function () {
-  return {
-    isPolling: { value: false },
-    setIsPolling: function (newValue) {
-      this.isPolling.value = newValue;
-    }
-  };
-});
