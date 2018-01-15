1c8f170b5e11709ab884a2c8c66a5a036ba8469e
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2018-01-15 09:19:07 +0100

    Better apps window

--- app/components/user-menu/user-menu-directive.js
+++ app/components/user-menu/user-menu-directive.js
@@ -2,150 +2,208 @@
  *
  * Shows user-menu and has logout login buttons
  */
-angular.module('user-menu')
-  .directive('userMenu',[
-    '$http',
-    'UtilService',
-    'MapService',
-    '$location',
-    'user',
-    '$uibModal',
-    'version',
-    'notie',
-    'gettextCatalog',
-    function (
-      $http,
-      UtilService,
-      MapService,
-      $location,
-      user,
-      $uibModal,
-      version,
-      notie,
-      gettextCatalog) {
-
-    var link = function (scope, element, attrs) {
+angular.module("user-menu").directive("userMenu", [
+  "$http",
+  "UtilService",
+  "MapService",
+  "$location",
+  "user",
+  "$uibModal",
+  "version",
+  "notie",
+  "gettextCatalog",
+  'State',
+  '$timeout',
+  function(
+    $http,
+    UtilService,
+    MapService,
+    $location,
+    user,
+    $uibModal,
+    version,
+    notie,
+    gettextCatalog,
+    State,
+    $timeout
+  ) {
+    var link = function(scope, element, attrs) {
 
       scope.user = user;
-      scope.showApps = false;
-
-      scope.modal = {
-        active: false,
-      };
-
+      scope.mustShowAppsButton = false;
+      scope.modal = { active: false };
       scope.inbox = [];
 
-      // Bind mapservice functions for zoom buttons;
-      scope.zoomIn = MapService.zoomIn;
-      scope.zoomOut = MapService.zoomOut;
+      // var appsButton = document.querySelector("#lizard-apps-button");
+      var appsContainer = document.querySelector("#lizard-apps-container");
+      var showAppsContainer = false;
 
-      angular.element('#lizard-apps-button').click(function (e) {
-        e.stopPropagation();
-      });
+      var CLASS_CONTAINER_ON_MAP = "lizard-apps-container-on-map";
+      var CLASS_CONTAINER_ON_DB = "lizard-apps-container-on-db";
 
-      angular.element(':not(#lizard-apps-button)').click(function () {
-        var lApps = document.querySelector('#lizard-apps-container');
-        if (!lApps.classList.contains('hidden')) {
-          lApps = document.querySelector('#lizard-apps-container');
-          lApps.classList.toggle('hidden');
+      if (State.context === 'map') {
+        appsContainer.classList.add(CLASS_CONTAINER_ON_MAP);
+      } else if (State.context === 'dashboard') {
+        appsContainer.classList.add(CLASS_CONTAINER_ON_DB);
+      }
+
+      scope.state = State;
+
+      scope.$watch('state.context', function (n, o) {
+        if (n === 'map') {
+          appsContainer.classList.remove(CLASS_CONTAINER_ON_DB);
+          appsContainer.classList.add(CLASS_CONTAINER_ON_MAP);
+        } else if (n === 'dashboard') {
+          appsContainer.classList.remove(CLASS_CONTAINER_ON_MAP);
+          appsContainer.classList.add(CLASS_CONTAINER_ON_DB);
         }
       });
+
+      angular.element("#lizard-apps-button").click(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        showAppsContainer = !showAppsContainer;
+      });
+
+      angular.element(":not(#lizard-apps-button)").click(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        if (showAppsContainer) {
+          appsContainer.classList.toggle('hidden');
+          showAppsContainer = false;
+        }
+      });
+
+      // appsButton.onclick = function (e) {
+      //   console.log("[CLICK] appsButton");
+      //   e.preventDefault();
+      //   e.stopPropagation();
+      //   // appsContainer.classList.toggle('hidden');
+      //   var containerClass = State.context === 'map'
+      //     ? CLASS_CONTAINER_ON_MAP
+      //     : CLASS_CONTAINER_ON_DB;
+
+      //   if (showAppsContainer) {
+      //     console.log("AAAA");
+      //     appsContainer.classList.toggle('hidden');
+      //     showAppsContainer = false;
+      //     appsContainer.classList.remove(containerClass);
+      //   } else {
+      //     console.log("BBBB");
+      //     appsContainer.classList.toggle('hidden');
+      //     showAppsContainer = true;
+      //     appsContainer.classList.add(containerClass);
+      //   }
+      // };
+
+      // angular.element("#lizard-apps-button").click(function (e) {
+      //   e.stopPropagation();
+      //   $timeout(function () {
+      //     console.log("aaaaaa");
+      //     var lApps = document.querySelector("#lizard-apps-container");
+      //     if (!lApps.classList.contains("hidden")) {
+      //       lApps.classList.toggle("hidden");
+      //     }
+      //   });
+
+      //   // if (!lApps.classList.contains("hidden")) {
+      //   //   console.log("<Mehkeee");
+      //   //   lApps = document.querySelector("#lizard-apps-container");
+      //   //   lApps.classList.toggle("hidden");
+      //   // }
+      // });
 
       /**
        * Turn off either favourites or apps when click the on or the other
        */
-      var toggleDashboardOrApps = function (e) {
-        var favs = e === true;
-        var lApps = document.querySelector('#lizard-apps-container');
-        if (!lApps.classList.contains('hidden') && favs) {
-          lApps.classList.toggle('hidden');
-        } else if (!favs) {
-          scope.favourites.enabled = false;
-        }
-      };
+      // var toggleDashboardOrApps = function (e) {
+      //   var favs = e === true;
+      //   var lApps = document.querySelector("#lizard-apps-container");
+      //   if (!lApps.classList.contains("hidden") && favs) {
+      //     lApps.classList.toggle("hidden");
+      //   } else if (!favs) {
+      //     scope.favourites.enabled = false;
+      //   }
+      // };
 
-      var appsScreenUrl = function () {
+      var appsScreenUrl = function() {
         var appsScreenSlug = UtilService.slugify($location.host());
         return "//apps.lizard.net/screens/" + appsScreenSlug + ".js";
       };
 
-      var script = document.createElement('script');
+      var script = document.createElement("script");
       script.src = appsScreenUrl();
-      script.onload = function () {
-        if (typeof window.Lizard.startPlugins === 'function') {
+      script.onload = function() {
+        if (typeof window.Lizard.startPlugins === "function") {
           window.Lizard.startPlugins(); // jshint ignore:line
-          scope.showApps = element
-            .find('#lizard-apps-button')
-            .children().length > 0;
+          scope.mustShowAppsButton =
+            element.find("#lizard-apps-button").children().length > 0;
           scope.$digest();
         }
       };
 
-      scope.$watch('favourites.enabled', toggleDashboardOrApps);
+      // scope.$watch("favourites.enabled", toggleDashboardOrApps);
 
       document.head.appendChild(script);
 
-      scope.openAbout = function (size) {
+      scope.openAbout = function(size) {
         var modalInstance = $uibModal.open({
           animation: true,
-          templateUrl: 'about.html',  // This is really the 'id' of the modal.
+          templateUrl: "about.html", // This is really the 'id' of the modal.
           size: size,
-          controller: function () {
+          controller: function() {
             this.version = version;
           },
-          controllerAs: 'about'
+          controllerAs: "about"
         });
       };
 
-      var domain = location.protocol +
-        '//' +
-        location.host.replace(':9000', ':8000') ;
+      var domain =
+        location.protocol + "//" + location.host.replace(":9000", ":8000");
 
-      var setLogOutUrl = function () {
-        window.location = '/accounts/logout/?domain=' +
+      var setLogOutUrl = function() {
+        window.location =
+          "/accounts/logout/?domain=" +
           domain +
-          '/&next=' +
+          "/&next=" +
           window.location.href;
       };
 
-      scope.logOut = function () {
+      scope.logOut = function() {
         notie.confirm(
-          gettextCatalog.getString(
-            "Are you sure you want to log out?"),
+          gettextCatalog.getString("Are you sure you want to log out?"),
           gettextCatalog.getString("Yes"),
           gettextCatalog.getString("No"),
           setLogOutUrl
         );
       };
 
-      scope.logIn = function () {
-        window.location = '/accounts/login/?domain=' +
+      scope.logIn = function() {
+        window.location =
+          "/accounts/login/?domain=" +
           domain +
-          '/&next=' +
+          "/&next=" +
           window.location.href;
       };
-
 
       /**
        * toggleExport - start the modal with export stuff
        */
-      scope.toggleExport = function () {
+      scope.toggleExport = function() {
         scope.modal.active = true;
       };
-
 
       /**
        * archiveMessage - archives a stored message based on id
        *
        * @param  {any} id string or integer representing the id e.g. "1"
        */
-      var archiveMessage = function (id) {
-        $http.post('/api/v3/inbox/{id}/read/'.replace(/\{id\}/g, id));
-        scope.inbox = scope.inbox.filter(function (message) {
+      var archiveMessage = function(id) {
+        $http.post("/api/v3/inbox/{id}/read/".replace(/\{id\}/g, id));
+        scope.inbox = scope.inbox.filter(function(message) {
           return message.id !== id;
         });
       };
-
 
       /**
        * goToMessageUrl - opens the URL that the message contains and archives
@@ -153,11 +211,10 @@ angular.module('user-menu')
        *
        * @param  {object} message as it comes from the server
        */
-      var goToMessageUrl = function (message) {
-        window.open(message.url, '_blank');
+      var goToMessageUrl = function(message) {
+        window.open(message.url, "_blank");
         archiveMessage(message.id);
       };
-
 
       /**
        * showMessage - show the message as a notie notification
@@ -165,38 +222,38 @@ angular.module('user-menu')
        *
        * @param  {object} message as it is retrieved from the server
        */
-      scope.showMessage = function (message) {
-        notie.confirm.apply(
-          message, [
+      scope.showMessage = function(message) {
+        notie.confirm.apply(message, [
           message.message,
           gettextCatalog.getString("Download"),
           gettextCatalog.getString("Archive Message"),
-          function () {
+          function() {
             goToMessageUrl(message);
-          }, function () {
-            archiveMessage(message.id);
           },
+          function() {
+            archiveMessage(message.id);
+          }
         ]);
       };
 
-      var requestAnimationFrame = window.requestAnimationFrame
-      || window.mozRequestAnimationFrame
-      || window.webkitRequestAnimationFrame
-      || window.msRequestAnimationFrame;
+      var requestAnimationFrame =
+        window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.msRequestAnimationFrame;
 
-      var cancelAnimationFrame = window.cancelAnimationFrame
-      || window.mozCancelAnimationFrame;
+      var cancelAnimationFrame =
+        window.cancelAnimationFrame || window.mozCancelAnimationFrame;
 
       /**
        * Retrieves messages from the server /inbox/ endpoint and put the
        * messages on the scope without being in the way of the browsers
        * rendering and only when the browser tab is active.
        */
-      var poll = requestAnimationFrame.bind(null, function () {
-        $http.get('/api/v3/inbox/')
-        .then(
-          function (response) { scope.inbox = response.data; }
-        );
+      var poll = requestAnimationFrame.bind(null, function() {
+        $http.get("/api/v3/inbox/").then(function(response) {
+          scope.inbox = response.data;
+        });
       });
 
       // do initial call
@@ -206,7 +263,7 @@ angular.module('user-menu')
        * getMessages - if user is authenticated, cancel previous request for
        * poll and request browser to schedule another poll.
        */
-      var getMessages = function () {
+      var getMessages = function() {
         if (user.authenticated) {
           cancelAnimationFrame(pollFrame);
           pollFrame = poll();
@@ -218,11 +275,15 @@ angular.module('user-menu')
       // poll for more messages
       setInterval(getMessages, POLL_INTERVAL);
 
+      // Bind mapservice functions for zoom buttons;
+      scope.zoomIn = MapService.zoomIn;
+      scope.zoomOut = MapService.zoomOut;
     };
 
     return {
-      restrict: 'E',
+      restrict: "E",
       link: link,
-      templateUrl: 'user-menu/user-menu.html'
+      templateUrl: "user-menu/user-menu.html"
     };
-  }]);
+  }
+]);
