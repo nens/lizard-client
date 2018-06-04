9992aecf57b2671fc3eb9f1784e626e2cd489301
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2018-06-04 15:22:28 +0200

    added th for surface_level; merge th's with same value

--- app/components/graph/chart-container-service.js
+++ app/components/graph/chart-container-service.js
@@ -12,6 +12,100 @@ angular.module('lizard-nxt')
     y: 'value'
   };
 
+  function addSurfaceLevelToThresholds (thresholds) {
+    var detectedSurfaceLevel = null,
+        referenceFrame,
+        assetName;
+
+    _.forEach(thresholds, function (v, k) {
+      if (detectedSurfaceLevel === null && v.surface_level) {
+        detectedSurfaceLevel = v.surface_level;
+        referenceFrame = v.reference_frame;
+        try {
+          assetName = v.name.split(":")[0];
+        } catch (e) {
+          assetName = "Asset";
+        }
+      }
+    });
+
+    if (detectedSurfaceLevel) {
+      thresholds.push({
+        name: assetName + ": surface level",
+        reference_frame: referenceFrame,
+        surface_level: detectedSurfaceLevel,
+        value: detectedSurfaceLevel
+      });
+    }
+
+    return thresholds;
+  }
+
+  function mergeOverlappingThresholds (thresholds) {
+    var overlappingThresholds = [],
+        nonOverlappingThresholds = [],
+        th0,
+        th1;
+
+    // Get overlapping thresholds (contains double occurrences):
+    for (var i = 0; i < thresholds.length; i++) {
+      th0 = thresholds[i];
+      for (var j = i + 1; j < thresholds.length; j++) {
+        th1 = thresholds[j];
+        if (th0.value === th1.value) {
+          overlappingThresholds.push(th0, th1);
+        }
+      }
+    }
+
+    // remove double occurrences from overlapping thresholds:
+    overlappingThresholds = _.uniqBy(overlappingThresholds, 'name');
+
+    // If no doubles were found, we're done and we can return all of the
+    // thresholds:
+    if (overlappingThresholds.length === 0) {
+      return thresholds;
+    }
+
+    // Get complement for overlapping thresholds, i.e. the non-overlapping
+    // thresholds:
+    thresholds.forEach(function (th) {
+      if (overlappingThresholds.indexOf(th) === -1) {
+        nonOverlappingThresholds.push(th);
+      }
+    });
+
+    // Merge the overlapping thresholds, i.e. build a single one with a single,
+    // merged name:
+    var mergedName;
+    try {
+      mergedName = overlappingThresholds[0].name.split(":")[0] + ":";
+    } catch (e) {
+      mergedName = "Asset:";
+    }
+
+    var varPart;
+    for (i = 0; i < overlappingThresholds.length; i++) {
+      try {
+        varPart = overlappingThresholds[i].name.split(":")[1];
+      } catch (e) {
+        varPart = "...";
+      }
+
+      if (i < overlappingThresholds.length - 1) {
+        varPart += " + ";
+      } else {
+        varPart += " ";
+      }
+
+      mergedName += varPart;
+    }
+
+    overlappingThresholds[0].name = mergedName;
+    nonOverlappingThresholds.push(overlappingThresholds[0]);
+    return nonOverlappingThresholds;
+  }
+
   /**
    * Charts are are small objects that are drawn on the graph canvas
    * They are a way to keep track of all of the lines or that are being drawn.
@@ -32,7 +126,10 @@ angular.module('lizard-nxt')
       this.unit = chartContent.unit;
 
       this.reference_frame = chartContent.reference_frame;
-      this.thresholds = chartContent.thresholds;
+
+      var newThresholds = addSurfaceLevelToThresholds(chartContent.thresholds);
+      this.thresholds = mergeOverlappingThresholds(newThresholds);
+
       this.description = chartContent.description;
       this.labels = chartContent.labels;
 
